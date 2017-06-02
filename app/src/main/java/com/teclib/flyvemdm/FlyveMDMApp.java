fa63c846d07edb6c08f4fd8baf0589ec4fb9989f
Rafael Hernandez <rafaelje@gmail.com>
2017-06-02 10:53:31 -0400

    chore(header) add header with GLPi v3 to all files

--- app/src/main/java/com/teclib/flyvemdm/FlyveMDMApp.java
+++ app/src/main/java/com/teclib/flyvemdm/FlyveMDMApp.java
@@ -1,3 +1,30 @@
+/*
+ *   Copyright © ${YEAR} Teclib. All rights reserved.
+ *
+ *   com.teclib.data is part of flyve-mdm-android
+ *
+ * flyve-mdm-android is a subproject of Flyve MDM. Flyve MDM is a mobile
+ * device management software.
+ *
+ * Flyve MDM is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Flyve MDM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Rafael Hernandez
+ * @date      02/06/2017
+ * @copyright Copyright © ${YEAR} Teclib. All rights reserved.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/flyve-mdm-android
+ * @link      https://flyve-mdm.com
+ * ------------------------------------------------------------------------------
+ */
+
 package com.teclib.flyvemdm;
 
 
