72f3756e831488222db0cad29f6f8a4b36a22330
Rafa Hernández <rhernandez@teclib.com>
2017-07-14 14:03:26 -0400

    just working on develop

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo $TRAVIS_BRANCH
-if [[ "$TRAVIS_BRANCH" == "feature/travis" ]];
+if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]];
 then
     fastlane android alpha storepass:'$KEYSTORE' keypass:'$ALIAS'
 fi
