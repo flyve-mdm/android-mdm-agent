de4dafa6de6f14d80ff61f3734f48e65a614ddcf
Rafa Hernández <rhernandez@teclib.com>
2017-07-14 18:21:08 -0400

    ready for deploy

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,19 +3,6 @@
 echo $TRAVIS_BRANCH
 if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]];
 then
-    fastlane android alpha storepass:'$KEYSTORE' keypass:'$ALIAS'
-fi
-
-if [[ "$TRAVIS_BRANCH" == "develop" ]];
-then
-    cd ci
-    tar -zxvf google.tar.gz
-    cd ..
-    fastlane android alpha storepass:'$KEYSTORE' keypass:'$ALIAS'
-fi
-
-if [[ "$TRAVIS_BRANCH" == "feature/travis" ]];
-then
     cd ci
     tar -zxvf google.tar.gz
     cd ..
