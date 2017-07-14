3a7adc5f3d967028e117da6a8db032dc4505ee99
Rafa Hernández <rhernandez@teclib.com>
2017-07-14 12:08:04 -0400

    add secure vars on travis and deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,5 +3,5 @@
 echo $TRAVIS_BRANCH
 if [[ "$TRAVIS_BRANCH" == "feature/travis" ]];
 then
-    fastlane android alpha storepass:'#KEYSTORE' keypass:'#ALIAS'
+    fastlane android alpha storepass:'$KEYSTORE' keypass:'$ALIAS'
 fi
