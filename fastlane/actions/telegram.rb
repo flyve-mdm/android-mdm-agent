f208fb0342f9fe80527f30f491b8629757df280e
Rafa Hernández <rhernandez@teclib.com>
2017-07-20 14:50:14 -0400

    ci(fastlane): Add send message on fastlane error

--- fastlane/actions/telegram.rb
+++ fastlane/actions/telegram.rb
@@ -11,7 +11,7 @@ module Fastlane
 
             req = Net::HTTP::Post.new(uri.request_uri)
             req.set_form_data({
-              'payload' => '{ "text": "' + params[:message] + "- *Branch*: " + Actions.git_branch + "- *Author*: " + Actions.git_author_email + '" }'
+              'payload' => '{ "text": "' + params[:message] + "\\n- *Branch*: " + Actions.git_branch + "\\n- *Author*: " + Actions.git_author_email + '" }'
             })
 
             response = https.request(req)
