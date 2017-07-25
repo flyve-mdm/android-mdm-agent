d2b3c1dd4635996a87927f67cc8bccf6a890cc7f
Rafa Hernández <rhernandez@teclib.com>
2017-07-25 14:58:11 -0400

    fix(history): close history back

--- app/src/main/java/org/flyve/mdm/agent/EnrollmentActivity.java
+++ app/src/main/java/org/flyve/mdm/agent/EnrollmentActivity.java
@@ -260,8 +260,9 @@ public class EnrollmentActivity extends AppCompatActivity {
      * Open the main activity
      */
     private void openMain() {
-        Intent miIntent = new Intent(EnrollmentActivity.this, MainActivity.class);
-        EnrollmentActivity.this.startActivity(miIntent);
+        Intent intent = new Intent(EnrollmentActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        EnrollmentActivity.this.startActivity(intent);
         setResult(RESULT_OK, null);
         EnrollmentActivity.this.finish();
     }
