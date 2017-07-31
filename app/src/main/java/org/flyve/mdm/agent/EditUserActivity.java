a1c5736c2108b7bbcb3498c48c947925dbf46943
Rafa Hernández <rhernandez@teclib.com>
2017-07-31 12:02:25 -0400

    feat(user): load data storage on user form

--- app/src/main/java/org/flyve/mdm/agent/EditUserActivity.java
+++ app/src/main/java/org/flyve/mdm/agent/EditUserActivity.java
@@ -1,7 +1,19 @@
 package org.flyve.mdm.agent;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.flyve.mdm.agent.data.DataStorage;
+import org.flyve.mdm.agent.utils.FlyveLog;
+import org.flyve.mdm.agent.utils.InputValidatorHelper;
 
 /*
  *   Copyright © 2017 Teclib. All rights reserved.
@@ -31,10 +43,114 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class EditUserActivity extends AppCompatActivity {
 
+    private TextView txtMessage;
+    private EditText editName;
+    private EditText editLastName;
+    private EditText editEmail;
+    private EditText editPhone;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enrollment);
+
+        DataStorage cache = new DataStorage(EditUserActivity.this);
+
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onBackPressed();
+                }
+            });
+        }
+
+        txtMessage = (TextView) findViewById(R.id.txtMessage);
+
+        editName = (EditText) findViewById(R.id.editName);
+        editName.setText( cache.getUserFirstName() );
+
+        editLastName = (EditText) findViewById(R.id.editLastName);
+        editLastName.setText( cache.getUserLastName() );
+
+        editEmail = (EditText) findViewById(R.id.editEmail);
+        editEmail.setText( cache.getUserEmail() );
+
+        editPhone = (EditText) findViewById(R.id.editPhone);
+        editPhone.setText( cache.getUserPhone() );
+
+        editPhone.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        editPhone.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    validateForm();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        ImageView btnRegister = (ImageView) findViewById(R.id.btnSave);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateForm();
+            }
+        });
     }
 
+    private void save() {
+        FlyveLog.d("Save information");
+    }
+
+    /**
+     * Send information to validateForm
+     */
+    private void validateForm() {
+        StringBuilder errMsg = new StringBuilder("Please fix the following errors and try again.\n\n");
+        txtMessage.setText("");
+
+        // Hide keyboard
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+
+        //Validate and Save
+        boolean allowSave = true;
+
+        String email = editEmail.getText().toString().trim();
+        String name = editName.getText().toString().trim();
+        String lastName = editLastName.getText().toString().trim();
+
+        // Email
+        if (InputValidatorHelper.isNullOrEmpty(name)) {
+            errMsg.append("- First name should not be empty.\n");
+            allowSave = false;
+        }
+
+        // First name
+        if (InputValidatorHelper.isNullOrEmpty(lastName)) {
+            errMsg.append("- Last name should not be empty.\n");
+            allowSave = false;
+        }
+
+        // Last name
+        if (email.equals("") || !InputValidatorHelper.isValidEmail(email)) {
+            errMsg.append("- Invalid email address.\n");
+            allowSave = false;
+        }
+
+        if(allowSave){
+            save();
+        } else {
+            txtMessage.setText(errMsg);
+        }
+    }
 }
