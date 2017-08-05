0e8b8f63258d03df02f8491fb00d6c508bc9c0ba
Rafa Hernández <rhernandez@teclib.com>
2017-08-05 17:55:34 -0400

    ci(travis): add auto increment on before script travis

---
+++ ci/before_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo TELEGRAM_WEBHOOKS=$TELEGRAM_WEBHOOKS > .env
+echo GIT_REPO=$TRAVIS_REPO_SLUG >> .env
+echo GIT_BRANCH=$TRAVIS_BRANCH >> .env
+
+echo $TRAVIS_BRANCH
+if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]];
+then
+    gradle increaseVersionCode
+    gradle incrementVersionName
+fi
