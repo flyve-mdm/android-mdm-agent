5f524c4519a41c0ff6b7da0d52f46b1fff76da4e
Rafa Hernández <rhernandez@teclib.com>
2017-08-05 16:11:29 -0400

    refactor(gradle): print version code and name on console

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,9 +2,6 @@
 
 echo $TRAVIS_BRANCH
 
-gradle increaseVersionCode
-gradle incrementVersionName
-
 if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]];
 then
     gradle increaseVersionCode
