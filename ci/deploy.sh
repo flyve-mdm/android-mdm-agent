df0282a9777730d7c76afa9ce891bc8d3bd05452
Rafa Hernández <rhernandez@teclib.com>
2017-08-05 17:55:34 -0400

    ci(travis): add auto increment on before script travis

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,9 +4,6 @@ echo $TRAVIS_BRANCH
 
 if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]];
 then
-    gradle increaseVersionCode
-    gradle incrementVersionName
-
     cd ci
     tar -zxvf google.tar.gz
     cd ..
