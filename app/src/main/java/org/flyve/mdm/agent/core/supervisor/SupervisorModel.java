5648013354bd198f5e65936acb6e6fd1b14034aa
Rafa Hernández <rhernandez@teclib.com>
2017-08-14 14:01:42 -0400

    refactor(supervisor): add new fields to supervisor model

--- app/src/main/java/org/flyve/mdm/agent/core/supervisor/SupervisorModel.java
+++ app/src/main/java/org/flyve/mdm/agent/core/supervisor/SupervisorModel.java
@@ -30,13 +30,17 @@ public class SupervisorModel {
 
     private String name;
     private String email;
+    private String phone;
+    private String website;
 
     public SupervisorModel() {
     }
 
-    public SupervisorModel(String name, String email) {
+    public SupervisorModel(String name, String email, String phone, String website) {
         this.name = name;
         this.email = email;
+        this.phone = phone;
+        this.website = website;
     }
 
     public String getName() {
@@ -53,5 +57,21 @@ public class SupervisorModel {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
     }
 }
