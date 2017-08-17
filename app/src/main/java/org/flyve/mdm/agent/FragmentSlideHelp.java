fc1297cb0808bac6dff1cb46d6096af5d7e38993
Rafa Hernández <rhernandez@teclib.com>
2017-08-17 10:28:05 -0400

    feat(help): add viewpager to create a slide help on splash

---
+++ app/src/main/java/org/flyve/mdm/agent/FragmentSlideHelp.java
@@ -0,0 +1,59 @@
+package org.flyve.mdm.agent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.flyve.mdm.agent.utils.Helpers;
+
+/*
+ *   Copyright © 2017 Teclib. All rights reserved.
+ *
+ *   This file is part of flyve-mdm-android
+ *
+ * flyve-mdm-android is a subproject of Flyve MDM. Flyve MDM is a mobile
+ * device management software.
+ *
+ * Flyve MDM is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Flyve MDM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    rafaelhernandez
+ * @date      17/8/17
+ * @copyright Copyright © 2017 Teclib. All rights reserved.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/flyve-mdm-android
+ * @link      https://flyve-mdm.com
+ * ------------------------------------------------------------------------------
+ */
+public class FragmentSlideHelp extends Fragment {
+
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            ViewGroup rootView = (ViewGroup) inflater.inflate(
+                    R.layout.fragment_help_step1, container, false);
+
+            final TextView txtLink = (TextView) rootView.findViewById(R.id.txtLink);
+            txtLink.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Helpers.openURL( FragmentSlideHelp.this.getContext(), txtLink.getText().toString() );
+                }
+            });
+
+            return rootView;
+        }
+
+
+}
