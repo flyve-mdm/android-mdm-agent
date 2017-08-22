c3e854be7290b98e7173a43e7eae02b4861c3158
Rafa Hernández <rhernandez@teclib.com>
2017-08-22 09:10:03 -0400

    ci(travis): rename google play file to deploy

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,7 +5,7 @@ echo $TRAVIS_BRANCH
 if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]];
 then
     cd ci
-    tar -zxvf google.tar.gz
+    tar -zxvf gplay.tar.gz
     cd ..
     fastlane android beta storepass:'$KEYSTORE' keypass:'$ALIAS'
 fi
