e85a38e3cb7910642fda3053530b5a89c38188c3
Rafa Hernández <rhernandez@teclib.com>
2017-08-22 16:04:10 -0400

    ci(tag): create tag with package version

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,10 +2,24 @@
 
 echo $TRAVIS_BRANCH
 
-if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]];
-then
+#if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]];
+#then
+    # uncompress cert file
     cd ci
     tar -zxvf gplay.tar.gz
     cd ..
+
+    # sign and deploy to store with fastlane
     fastlane android beta storepass:'$KEYSTORE' keypass:'$ALIAS'
-fi
+
+    # create a new release on git
+    git config --global user.email $GH_EMAIL
+    git config --global user.name "Flyve MDM"
+
+    #create a git tag
+    export GIT_TAG=$(cat package.json | jq '.version')
+    git tag $GIT_TAG -a -m "build(tag): Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
+
+    #send tag to git hub
+    git push -q https://$GH_USER:${GH_TOKEN}@github.com/flyve-mdm/flyve-mdm-android-agent.git --tags
+#fi
