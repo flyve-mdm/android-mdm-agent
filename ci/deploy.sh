53a697abd1f9a3c92090feb0ddcc86c6d257df67
Rafa Hernández <rhernandez@teclib.com>
2017-08-23 08:45:59 -0400

    ci(travis): create tag script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -12,15 +12,15 @@ echo $TRAVIS_BRANCH
     # sign and deploy to store with fastlane
     fastlane android beta storepass:'$KEYSTORE' keypass:'$ALIAS'
 
-    conventional-github-releaser -t $GH_TOKEN
     # create a new release on git
     #git config --global user.email $GH_EMAIL
     #git config --global user.name "Flyve MDM"
 
     #create a git tag
-    #export GIT_TAG=$(cat package.json | jq '.version')
-    #git tag $GIT_TAG -a -m "build(tag): Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
+    export GIT_TAG=$(jq -r ".version" package.json)
+    git tag $GIT_TAG -a -m "build(tag): Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
 
     #send tag to git hub
     #git push -q https://$GH_USER:${GH_TOKEN}@github.com/flyve-mdm/flyve-mdm-android-agent.git --tags
+    conventional-github-releaser -t $GH_TOKEN
 #fi
