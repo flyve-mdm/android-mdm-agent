749fddc5cc8ba5998cbf13ffe5969249c756ed04
Rafa Hernández <rhernandez@teclib.com>
2017-08-23 08:48:52 -0400

    ci(travis): run standard-version

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -5,8 +5,9 @@ echo GIT_REPO=$TRAVIS_REPO_SLUG >> .env
 echo GIT_BRANCH=$TRAVIS_BRANCH >> .env
 
 echo $TRAVIS_BRANCH
-if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]];
-then
+#if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]];
+#then
+    npm run release
     gradle increaseVersionCode
     gradle incrementVersionName
-fi
+#fi
