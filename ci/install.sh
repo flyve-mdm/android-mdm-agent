5b7a1363365764cf9a932a8e28e1ec1426b42f3f
Rafa Hernández <rhernandez@teclib.com>
2017-08-24 10:22:48 -0400

    ci(travis): add license to travis script files

--- ci/install.sh
+++ ci/install.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+#   Copyright © 2017 Teclib. All rights reserved.
+#
+#   This file is part of flyve-mdm-android-agent
+#
+# flyve-mdm-android-agent is a subproject of Flyve MDM. Flyve MDM is a mobile
+# device management software.
+#
+# Flyve MDM is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# Flyve MDM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# ------------------------------------------------------------------------------
+# @author    Rafael Hernandez - rafaelje
+# @date      24/9/17
+# @copyright Copyright © 2017 Teclib. All rights reserved.
+# @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+# @link      https://github.com/flyve-mdm/flyve-mdm-android-agent
+# @link      https://flyve-mdm.com
+# ------------------------------------------------------------------------------
+
 #-----------------------------------------------------------------
 # DEVELOP MASTER and NOT PULLREQUEST
 #-----------------------------------------------------------------
