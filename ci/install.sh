b2d08991c8367022b888d5b30d45b4d257db5504
Rafa Hernandez <rhernandez@teclib.com>
2017-08-28 08:34:00 -0400

    ci(travis): install global node virtual manager

--- ci/install.sh
+++ ci/install.sh
@@ -36,12 +36,10 @@ if [[ ("$TRAVIS_BRANCH" == "develop" ||  "$TRAVIS_BRANCH" == "master") && "$TRAV
     # install fastlane
     gem install fastlane
 
-    # install node_js
-    rm -rf ~/.nvm && git clone https://github.com/creationix/nvm.git ~/.nvm && (cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`) && source ~/.nvm/nvm.sh && nvm install $TRAVIS_NODE_VERSION
+    # install globally
+    npm install -g conventional-github-releaser
 
     # install node package available on package.json
     npm install
 
-    # install globally
-    npm install -g conventional-github-releaser
 fi
