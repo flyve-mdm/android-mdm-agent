37f4beac4381dfce48d3f1d33abf2d88e99e8019
Gianfranco Manganiello <gianfrancomanganiello@hotmail.com>
2017-08-29 10:09:49 -0400

    docs(home): change design of the instructions

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ self.addEventListener('install', function(e) {
   
   {% assign name = site.github.project_title | replace: "flyve-mdm-", "" %} 
   
-  var CACHE_NAME = '{{name}}-version-1'
+  var CACHE_NAME = '{{name}}-version-2'
 
   caches.keys().then(function(cacheNames) {
     return Promise.all(
