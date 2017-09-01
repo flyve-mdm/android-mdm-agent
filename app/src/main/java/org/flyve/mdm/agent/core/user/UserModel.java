160b68ebf4341ef3d31b231bbce354e78a740db0
Naylin <nmedina@teclib.com>
2017-09-01 17:46:55 -0400

    docs(setPhone): add the param of the tag

--- app/src/main/java/org/flyve/mdm/agent/core/user/UserModel.java
+++ app/src/main/java/org/flyve/mdm/agent/core/user/UserModel.java
@@ -115,6 +115,10 @@ public class UserModel {
         return phone;
     }
 
+    /**
+     * Set the phone of the user
+     * @param string the phone
+     */
     public void setPhone(String phone) {
         this.phone = phone;
     }
