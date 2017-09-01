6568048b38d5e49cd54de65aed7e3eccc9b216dd
Naylin <nmedina@teclib.com>
2017-09-01 18:02:27 -0400

    docs(setAdministrativeNumber): add the param of the tag

--- app/src/main/java/org/flyve/mdm/agent/core/user/UserModel.java
+++ app/src/main/java/org/flyve/mdm/agent/core/user/UserModel.java
@@ -163,6 +163,10 @@ public class UserModel {
         return administrativeNumber;
     }
 
+    /**
+     * Set the administrative number of the user
+     * @param string administrativeNumber
+     */
     public void setAdministrativeNumber(String administrativeNumber) {
         this.administrativeNumber = administrativeNumber;
     }
