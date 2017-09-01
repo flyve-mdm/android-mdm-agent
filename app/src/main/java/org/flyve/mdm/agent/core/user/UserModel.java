2a67e927ecd23cea15994b983f136514e95cddff
Naylin <nmedina@teclib.com>
2017-09-01 17:55:36 -0400

    docs(getPhone2): add the return of the tag

--- app/src/main/java/org/flyve/mdm/agent/core/user/UserModel.java
+++ app/src/main/java/org/flyve/mdm/agent/core/user/UserModel.java
@@ -139,6 +139,10 @@ public class UserModel {
         this.mobilePhone = mobilePhone;
     }
 
+    /**
+     * Get a second phone of the user
+     * @return the second phone 
+     */
     public String getPhone2() {
         return phone2;
     }
