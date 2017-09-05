d13e0eba073ef8c8043b258e2a218b50d053a947
Naylin <nmedina@teclib.com>
2017-09-05 16:47:45 -0400

    docs(getImage): add the return of the tag

--- app/src/main/java/org/flyve/mdm/agent/core/walkthrough/WalkthroughModel.java
+++ app/src/main/java/org/flyve/mdm/agent/core/walkthrough/WalkthroughModel.java
@@ -76,6 +76,10 @@ public class WalkthroughModel {
         this.link = link;
     }
 
+    /**
+     * Get the image of the Walkthrough
+     * @return int the image
+     */
     public int getImage() {
         return image;
     }
