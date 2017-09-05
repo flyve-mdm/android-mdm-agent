57fa3a44362396a1a0a762f058aad85af66776b5
Naylin <nmedina@teclib.com>
2017-09-05 18:23:11 -0400

    docs(getManifestVersion): add the return of the tag

--- app/src/main/java/org/flyve/mdm/agent/data/DataStorage.java
+++ app/src/main/java/org/flyve/mdm/agent/data/DataStorage.java
@@ -71,6 +71,10 @@ public class DataStorage {
 		setData("onlineStatus", String.valueOf(status));
 	}
 
+	/**
+	 * Get the version of the Manifest
+	 * @return string the Manifest version
+	 */
 	public String getManifestVersion() {
 		return getData("manifestVersion");
 	}
