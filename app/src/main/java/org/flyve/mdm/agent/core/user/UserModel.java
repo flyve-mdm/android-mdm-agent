9b2fa61c11a55b4012944e81205f4107ae47e8f7
Naylin <nmedina@teclib.com>
2017-09-05 15:50:30 -0400

    docs(setEmail): add the param of the tag

--- app/src/main/java/org/flyve/mdm/agent/core/user/UserModel.java
+++ app/src/main/java/org/flyve/mdm/agent/core/user/UserModel.java
@@ -220,6 +220,10 @@ public class UserModel {
             return email;
         }
 
+        /**
+         * Set the email of the user
+         * @param string the email
+         */
         public void setEmail(String email) {
             this.email = email;
         }
