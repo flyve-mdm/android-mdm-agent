6dab3bc7e99a4a99399ff7888bf1d79196749c9d
Naylin <nmedina@teclib.com>
2017-09-06 10:42:26 -0400

    docs(setName): add the param of the tag

--- app/src/main/java/org/flyve/mdm/agent/data/DataStorage.java
+++ app/src/main/java/org/flyve/mdm/agent/data/DataStorage.java
@@ -303,6 +303,10 @@ public class DataStorage {
 		return getData("name");
 	}
 
+	/**
+	 * Set the name
+	 * @param string the name
+	 */
 	public void setName(String name) {
 		setData("name", name);
 	}
