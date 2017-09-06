5f155fe6d81aa83a2b2befab47143c6f3639a120
Naylin <nmedina@teclib.com>
2017-09-06 10:06:33 -0400

    docs(getTls): add the return of the tag

--- app/src/main/java/org/flyve/mdm/agent/data/DataStorage.java
+++ app/src/main/java/org/flyve/mdm/agent/data/DataStorage.java
@@ -215,6 +215,10 @@ public class DataStorage {
 		setData("port", port);
 	}
 
+	/**
+	 * Get the Transport Layer Security (TLS)
+	 * @return string the TLS
+	 */
 	public String getTls() {
 		return getData("tls");
 	}
