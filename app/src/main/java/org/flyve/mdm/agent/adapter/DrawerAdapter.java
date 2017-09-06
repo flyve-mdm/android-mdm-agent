db353c30516daddf73a0132bc7b2412091966d6c
Naylin <nmedina@teclib.com>
2017-09-06 14:04:12 -0400

    docs(getCount): add the return of the tag

--- app/src/main/java/org/flyve/mdm/agent/adapter/DrawerAdapter.java
+++ app/src/main/java/org/flyve/mdm/agent/adapter/DrawerAdapter.java
@@ -52,6 +52,10 @@ public class DrawerAdapter extends BaseAdapter {
 		inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	}
 
+	/**
+	 * Get count of the data
+	 * @return int the data size 
+	 */
 	@Override
 	public int getCount() {
 		return this.data.size();
