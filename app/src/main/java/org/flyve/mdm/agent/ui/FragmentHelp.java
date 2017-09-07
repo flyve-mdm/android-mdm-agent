092cfd55000eda140c688488a6dc43720a8c561f
Naylin <nmedina@teclib.com>
2017-09-07 10:51:07 -0400

    docs(onCreateView): add the param and return tags

--- app/src/main/java/org/flyve/mdm/agent/ui/FragmentHelp.java
+++ app/src/main/java/org/flyve/mdm/agent/ui/FragmentHelp.java
@@ -46,6 +46,13 @@ public class FragmentHelp extends Fragment {
     private ProgressDialog progressBar;
     private static final String HELP_URL = "http://flyve-mdm.com";
 
+    /**
+     * Instantiate the user interface view
+     * @param LayoutInflater inflater the object that can be used to inflate any views in the fragment
+     * @param ViewGroup the parent view the fragment's UI should be attached to
+     * @param Bundle this fragment is being re-constructed from a previous saved state
+     * @return View the view for the fragment's UI
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
