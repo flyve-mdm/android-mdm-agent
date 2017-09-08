c736d09d823185a2272b0646d9481b8cb934333b
Naylin <nmedina@teclib.com>
2017-09-08 15:44:30 -0400

    docs(onCreate): add the param of the tag

--- app/src/main/java/org/flyve/mdm/agent/ui/PreviewSupervisorActivity.java
+++ app/src/main/java/org/flyve/mdm/agent/ui/PreviewSupervisorActivity.java
@@ -39,6 +39,11 @@ import org.flyve.mdm.agent.utils.Helpers;
  */
 public class PreviewSupervisorActivity extends AppCompatActivity {
 
+    /**
+     * Called when the activity is starting
+     * It shows the UI with the Supervisor information
+     * @param Bundle if the activity is being re-initialized, it contains the data it most recently supplied
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
