59091915b0c0af051a4ac2ac62fae5a1bfbad49c
Naylin <nmedina@teclib.com>
2017-09-08 17:38:14 -0400

    docs(onCreate): add the param of the tag

--- app/src/main/java/org/flyve/mdm/agent/ui/StartEnrollmentActivity.java
+++ app/src/main/java/org/flyve/mdm/agent/ui/StartEnrollmentActivity.java
@@ -53,6 +53,11 @@ public class StartEnrollmentActivity extends Activity {
     private ProgressBar pb;
     private static final int REQUEST_EXIT = 1;
 
+    /**
+     * Called when the activity is starting
+     * It shows the UI to start the enrollment
+     * @param Bundle if the activity is being re-initialized, it contains the data it most recently supplied
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
