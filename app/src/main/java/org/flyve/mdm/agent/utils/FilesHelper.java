f9d9eecf9d4d6063b377a97b01279622081ea69f
Naylin <nmedina@teclib.com>
2017-09-11 11:44:07 -0400

    docs(getUpkDir): add the return and throws tags

--- app/src/main/java/org/flyve/mdm/agent/utils/FilesHelper.java
+++ app/src/main/java/org/flyve/mdm/agent/utils/FilesHelper.java
@@ -74,6 +74,11 @@ public class FilesHelper {
         return System.getenv("EXTERNAL_STORAGE");
     }
 
+    /**
+     * Get the directory of the UPK
+     * @return string the UPK directory
+     * @throws Exception
+     */
     private static String getUpkDir() throws Exception {
         FlyveLog.d(System.getenv("EXTERNAL_STORAGE") + "/.fdroid/");
         return System.getenv("EXTERNAL_STORAGE") + "/.fdroid/";
