5b7e802902daf40e1dccb7c14f2fc3a3839ff80e
Naylin <nmedina@teclib.com>
2017-09-11 12:44:12 -0400

    docs(addLine): add the param and throws tags

--- app/src/main/java/org/flyve/mdm/agent/utils/LogFileReader.java
+++ app/src/main/java/org/flyve/mdm/agent/utils/LogFileReader.java
@@ -54,6 +54,11 @@ public class LogFileReader {
     private LogFileReader() {
     }
 
+    /**
+     * Add a new line
+     * @param string the line to add
+     * @throws Exception an error message
+     */
     private static void addLine(String line) {
         try {
             HashMap<String, String> map = new HashMap<>();
