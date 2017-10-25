23f73a1738730bf781cc11df16465fa34defd0cf
Rafa Hernandez <rhernandez@teclib.com>
2017-10-25 10:38:11 -0400

    feat(about): add screen with about information

---
+++ app/src/main/java/org/flyve/mdm/agent/ui/FragmentAbout.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2017 Teclib'
+ *
+ * This file is part of Flyve MDM Inventory Agent Android.
+ *
+ * Flyve MDM Inventory Agent Android is a subproject of Flyve MDM. Flyve MDM is a mobile
+ * device management software.
+ *
+ * Flyve MDM Android is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Flyve MDM Inventory Agent Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Rafael Hernandez - rafaelje
+ * @copyright Copyright (c) 2017 Flyve MDM
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/flyve-mdm-android-inventory-agent/
+ * @link      http://www.glpi-project.org/
+ * @link      https://flyve-mdm.com/
+ * ------------------------------------------------------------------------------
+ */
+package org.flyve.mdm.agent.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import org.flyve.mdm.agent.R;
+import org.flyve.mdm.agent.utils.EnvironmentInfo;
+
+public class FragmentAbout extends Fragment {
+
+    private int countEasterEgg;
+
+    /**
+     * Called when the activity is starting, inflates the activity's UI
+     * @param Bundle savedInstanceState if the activity is re-initialized, it contains the data it most recently supplied
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.activity_about, container, false);
+
+        TextView txtAbout = (TextView) v.findViewById(R.id.txtAbout);
+
+        EnvironmentInfo enviromentInfo = new EnvironmentInfo(FragmentAbout.this.getContext());
+
+        if(enviromentInfo.getIsLoaded()) {
+            txtAbout.setText(Html.fromHtml(aboutStr(enviromentInfo.getVersion(), enviromentInfo.getBuild(), enviromentInfo.getDate(), enviromentInfo.getCommit(), enviromentInfo.getCommitFull(), enviromentInfo.getGithub())));
+            txtAbout.setMovementMethod(LinkMovementMethod.getInstance());
+        } else {
+            txtAbout.setVisibility(View.GONE);
+        }
+
+        return v;
+    }
+
+    private String aboutStr(String version, String build, String date, String commit, String commitFull, String github) {
+        String str = "Flyve MDM Agent, version "+ version +", build "+ build +".<br />";
+        str += "Built on "+ date +". Last commit <a href='"+github+"/commit/"+commitFull+"'>"+ commit +"</a>.<br />";
+        str += "© <a href='http://teclib-edition.com/'>Teclib'</a> 2017. Licensed under <a href='https://www.gnu.org/licenses/gpl-3.0.en.html'>GPLv3</a>. <a href='https://flyve-mdm.com/'>Flyve MDM</a>®";
+
+        return str;
+    }
+}
