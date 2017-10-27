d0e2285da2daf48c5d08408f63034b1998dc56ac
Rafa Hernandez <rhernandez@teclib.com>
2017-10-27 07:42:19 -0400

    build(inventory): update inventory library

--- app/src/main/java/org/flyve/mdm/agent/utils/MQTTHelper.java
+++ app/src/main/java/org/flyve/mdm/agent/utils/MQTTHelper.java
@@ -29,21 +29,18 @@ package org.flyve.mdm.agent.utils;
 
 import android.content.Context;
 import android.location.Location;
-
-import com.flyvemdm.inventory.InventoryTask;
-
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.flyve.inventory.InventoryTask;
 import org.flyve.mdm.agent.BuildConfig;
 import org.flyve.mdm.agent.data.DataStorage;
 import org.flyve.mdm.agent.security.FlyveDeviceAdminUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 
 public class MQTTHelper {
