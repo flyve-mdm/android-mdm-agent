d205a9f535dcdfa5760593c333e11219375bce3c
Rafa Hernandez <rhernandez@teclib.com>
2017-11-05 09:58:17 -0400

    refactor(getUnixTime): convert date to UTC

--- app/src/test/java/org/flyve/inventory/agent/ExampleUnitTest.java
+++ app/src/test/java/org/flyve/inventory/agent/ExampleUnitTest.java
@@ -3,7 +3,6 @@ package org.flyve.inventory.agent;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
-import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
