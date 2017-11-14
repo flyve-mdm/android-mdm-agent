41f6abdef1cebc2fa256058aa71575967209cd01
Rafa Hernandez <rhernandez@teclib.com>
2017-11-14 13:33:37 -0400

    feat(lockscreen): add notifications receiver for lock screen

--- app/src/main/java/org/flyve/mdm/agent/services/LockScreenReceiver.java
+++ app/src/main/java/org/flyve/mdm/agent/services/LockScreenReceiver.java
@@ -1,9 +1,11 @@
-package org.example.lockscreen;
+package org.flyve.mdm.agent.services;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+
+import org.flyve.mdm.agent.ui.MainActivity;
 
 public class LockScreenReceiver extends BroadcastReceiver {
     @Override
