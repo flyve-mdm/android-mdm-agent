165cd87bff3343566beac04f8e38f06ac2343ae5
Rafa Hernandez <rhernandez@teclib.com>
2017-11-14 13:32:37 -0400

    feat(lockscreen): add lockscreen service

---
+++ app/src/main/java/org/flyve/mdm/agent/services/LockScreenService.java
@@ -0,0 +1,39 @@
+package org.flyve.mdm.agent.services;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+
+public class LockScreenService extends Service {
+
+    BroadcastReceiver receiver;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        //Start listening for the Screen On, Screen Off, and Boot completed actions
+        IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        filter.addAction(Intent.ACTION_BOOT_COMPLETED);
+
+        //Set up a receiver to listen for the Intents in this Service
+        receiver = new LockScreenReceiver();
+        registerReceiver(receiver, filter);
+
+        return LockScreenService.START_STICKY;
+    }
+
+
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(receiver);
+        super.onDestroy();
+    }
+}
