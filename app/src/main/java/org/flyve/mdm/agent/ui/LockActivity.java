4f5c533c9ef4e42bfdbb4155c77548c294c140cb
Rafa Hernandez <rhernandez@teclib.com>
2017-12-14 10:13:41 -0400

    refactor(policy): improve lock screen

--- app/src/main/java/org/flyve/mdm/agent/ui/LockActivity.java
+++ app/src/main/java/org/flyve/mdm/agent/ui/LockActivity.java
@@ -1,134 +1,39 @@
 package org.flyve.mdm.agent.ui;
 
-import android.app.Activity;
-import android.app.ActivityManager;
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.graphics.PixelFormat;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.EditText;
+
 import org.flyve.mdm.agent.R;
-import org.flyve.mdm.agent.core.user.UserController;
-import org.flyve.mdm.agent.core.user.UserModel;
-import java.util.ArrayList;
-import java.util.List;
+import org.flyve.mdm.agent.services.LockScreenService;
 
-public class LockActivity extends Activity {
-
-    private WindowManager wm;
-    private PackageManager packageManager;
-    private int flags; // Window flags
-    private View disableStatusBar;
-
-    // Components for home launcher activity
-    private ComponentName cnHome;
-    private int componentEnabled;
-    private EditText editEmail;
+public class LockActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        IntentFilter filter = new IntentFilter("org.flyve.mdm.agent.unlock");
-        this.registerReceiver(new Receiver(), filter);
+        startService(new Intent(this,LockScreenService.class));
 
-        // Activity window flags
-        flags = WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
-                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
-                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
-                WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH;
-
-        disableStatusBar = new View(this);
-
-        // Get screen density and calculate disableStatusBar view height
-        float screenDensity = getResources().getDisplayMetrics().density;
-
-        float disableStatusBar_height = screenDensity * 70;
-
-        // DisableStatusBar view parameters
-        WindowManager.LayoutParams handleParamsTop = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.MATCH_PARENT,
-                (int) disableStatusBar_height,
+        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.TYPE_SYSTEM_ERROR,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
-                        WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
-                        WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
-                PixelFormat.TRANSPARENT);
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN,
+                PixelFormat.TRANSLUCENT);
 
-        handleParamsTop.gravity = Gravity.TOP | Gravity.CENTER;
+        WindowManager wm = (WindowManager) getApplicationContext()
+                .getSystemService(Context.WINDOW_SERVICE);
 
-        packageManager = getPackageManager();
-        componentEnabled = PackageManager.COMPONENT_ENABLED_STATE_ENABLED;
+        ViewGroup mTopView = (ViewGroup) getLayoutInflater().inflate(R.layout.activity_lock, null);
+        getWindow().setAttributes(params);
+        wm.addView(mTopView, params);
 
-        cnHome = new ComponentName(this, "org.flyve.mdm.agent.LockHome");
-
-        // Enable home launcher activity component
-        packageManager.setComponentEnabledSetting(cnHome, componentEnabled, PackageManager.DONT_KILL_APP);
-
-        // Get window manager service, add the window flags and the disableStatusBar view
-        wm = (WindowManager) getApplicationContext().getSystemService(WINDOW_SERVICE);
-        this.getWindow().addFlags(flags);
-        wm.addView(disableStatusBar, handleParamsTop);
-
-        // If Android version 4.4 or bigger, add translucent status bar flag
-        if (Build.VERSION.SDK_INT >= 19) {
-            this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        }
-
-        // When disableStatusBar view touched, consume touch
-        disableStatusBar.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return true;
-            }
-        });
-
-        setContentView(R.layout.activity_lock);
-
-        editEmail = (EditText) findViewById(R.id.editEmail);
-
-        // Check default launcher, if current package isn't the default one
-        // Open the 'Select home app' dialog for user to pick default home launcher
-        if (!isMyLauncherDefault()) {
-            packageManager.clearPackagePreferredActivities(getPackageName());
-
-            Intent launcherPicker = new Intent();
-            launcherPicker.setAction(Intent.ACTION_MAIN);
-            launcherPicker.addCategory(Intent.CATEGORY_HOME);
-            startActivity(launcherPicker);
-        }
-    }
-
-    // Method to check whether the package is the default home launcher or not
-    public boolean isMyLauncherDefault() {
-        final IntentFilter launcherFilter = new IntentFilter(Intent.ACTION_MAIN);
-        launcherFilter.addCategory(Intent.CATEGORY_HOME);
-
-        List<IntentFilter> filters = new ArrayList<IntentFilter>();
-        filters.add(launcherFilter);
-
-        final String myPackageName = getPackageName();
-        List<ComponentName> activities = new ArrayList<ComponentName>();
-
-        packageManager.getPreferredActivities(filters, activities, "org.flyve.mdm.agent");
-
-        for (ComponentName activity : activities) {
-            if (myPackageName.equals(activity.getPackageName())) {
-                return true;
-            }
-        }
-
-        return false;
     }
 
     @Override
@@ -138,25 +43,7 @@ public class LockActivity extends Activity {
 
     public void unlockScreen(View view) {
         //Instead of using finish(), this totally destroys the process
-        UserModel user = new UserController(LockActivity.this).getCache();
-
-        if(editEmail.getText().toString().equals(user.getEmails().get(0).getEmail())) {
-            closeLock();
-        }
-    }
-
-    private void closeLock() {
         android.os.Process.killProcess(android.os.Process.myPid());
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        ActivityManager activityManager = (ActivityManager) getApplicationContext()
-                .getSystemService(Context.ACTIVITY_SERVICE);
-
-        activityManager.moveTaskToFront(getTaskId(), 0);
     }
 
     // Handle button clicks
@@ -166,12 +53,16 @@ public class LockActivity extends Activity {
         if ((keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)
                 || (keyCode == KeyEvent.KEYCODE_POWER)
                 || (keyCode == KeyEvent.KEYCODE_VOLUME_UP)
-                || (keyCode == KeyEvent.KEYCODE_CAMERA)
-                || (keyCode == KeyEvent.KEYCODE_HOME)) {
+                || (keyCode == KeyEvent.KEYCODE_CAMERA)) {
+            return true;
+        }
+        if ((keyCode == KeyEvent.KEYCODE_HOME)) {
+
             return true;
         }
 
         return false;
+
     }
 
     // handle the key press events here itself
@@ -180,19 +71,11 @@ public class LockActivity extends Activity {
                 || (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_DOWN)
                 || (event.getKeyCode() == KeyEvent.KEYCODE_POWER)) {
             return false;
-        } else if ((event.getKeyCode() == KeyEvent.KEYCODE_HOME)) {
+        }
+        if ((event.getKeyCode() == KeyEvent.KEYCODE_HOME)) {
+
             return true;
         }
         return false;
-    }
-
-    private class Receiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if("unlock".equalsIgnoreCase(action)) {
-                closeLock();
-            }
-        }
     }
 }
