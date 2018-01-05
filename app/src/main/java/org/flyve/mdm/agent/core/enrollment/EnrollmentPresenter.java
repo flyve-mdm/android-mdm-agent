de8f213ba3a8c7966da9230405b55db9d21c075c
Rafa Hernandez <rhernandez@teclib.com>
2018-01-05 12:29:43 -0400

    refactor(enroll): update email on model

--- app/src/main/java/org/flyve/mdm/agent/core/enrollment/EnrollmentPresenter.java
+++ app/src/main/java/org/flyve/mdm/agent/core/enrollment/EnrollmentPresenter.java
@@ -87,7 +87,7 @@ public class EnrollmentPresenter implements Enrollment.Presenter {
     }
 
     @Override
-    public void enroll(Context context, List<UserModel.EmailsData> arrEmails, String invitationToken, String firstName, String lastName, String phone, String phone2, String mobilePhone, String inventory, String photo, String language, String administrativeNumber) {
-        model.enroll(context, arrEmails, invitationToken, firstName, lastName, phone, phone2, mobilePhone, inventory, photo, language, administrativeNumber);
+    public void enroll(Context context, List<UserModel.EmailsData> arrEmails, String firstName, String lastName, String phone, String phone2, String mobilePhone, String inventory, String photo, String language, String administrativeNumber) {
+        model.enroll(context, arrEmails, firstName, lastName, phone, phone2, mobilePhone, inventory, photo, language, administrativeNumber);
     }
 }
