426f03e8ab0ee8fbed50058fc7a41ad587edf84b
Rafa Hernandez <rhernandez@teclib.com>
2018-01-12 18:40:31 -0400

    refactor(deeplink): create schema

--- app/src/main/java/org/flyve/mdm/agent/core/deeplink/DeeplinkSchema.java
+++ app/src/main/java/org/flyve/mdm/agent/core/deeplink/DeeplinkSchema.java
@@ -27,4 +27,71 @@ package org.flyve.mdm.agent.core.deeplink;
  * ------------------------------------------------------------------------------
  */
 public class DeeplinkSchema {
+
+    private String url = "";
+    private String userToken = "";
+    private String invitationToken = "";
+    private String name = "";
+    private String phone = "";
+    private String website = "";
+    private String email = "";
+
+    public DeeplinkSchema() {
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUserToken() {
+        return userToken;
+    }
+
+    public void setUserToken(String userToken) {
+        this.userToken = userToken;
+    }
+
+    public String getInvitationToken() {
+        return invitationToken;
+    }
+
+    public void setInvitationToken(String invitationToken) {
+        this.invitationToken = invitationToken;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
