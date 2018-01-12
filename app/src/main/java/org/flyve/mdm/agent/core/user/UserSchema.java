62c4d01dfd86041d7ae36c8a7452810647d535bc
Rafa Hernandez <rhernandez@teclib.com>
2018-01-12 13:13:38 -0400

    refactor(user): create schema

---
+++ app/src/main/java/org/flyve/mdm/agent/core/user/UserSchema.java
@@ -0,0 +1,120 @@
+package org.flyve.mdm.agent.core.user;
+
+import org.flyve.mdm.agent.data.UserData;
+
+import java.util.List;
+
+/*
+ *   Copyright © 2018 Teclib. All rights reserved.
+ *
+ *   This file is part of flyve-mdm-android
+ *
+ * flyve-mdm-android is a subproject of Flyve MDM. Flyve MDM is a mobile
+ * device management software.
+ *
+ * Flyve MDM is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Flyve MDM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Rafael Hernandez
+ * @date      12/1/18
+ * @copyright Copyright © 2018 Teclib. All rights reserved.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/flyve-mdm-android
+ * @link      https://flyve-mdm.com
+ * ------------------------------------------------------------------------------
+ */
+
+public class UserSchema {
+
+    private List<UserData.EmailsData> emails;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String phone2;
+    private String mobilePhone;
+    private String picture;
+    private String language;
+    private String administrativeNumber;
+
+    public UserSchema() {
+    }
+
+    public List<UserData.EmailsData> getEmails() {
+        return emails;
+    }
+
+    public void setEmails(List<UserData.EmailsData> emails) {
+        this.emails = emails;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public void setPhone2(String phone2) {
+        this.phone2 = phone2;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getAdministrativeNumber() {
+        return administrativeNumber;
+    }
+
+    public void setAdministrativeNumber(String administrativeNumber) {
+        this.administrativeNumber = administrativeNumber;
+    }
+}
