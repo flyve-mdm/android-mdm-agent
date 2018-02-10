648c10dd2c9bc99c99918b5a900d57e9117bce6c
Rafa Hernandez <rhernandez@teclib.com>
2018-02-10 14:14:04 -0400

    feat(database): add database setup
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- app/src/main/java/org/flyve/mdm/agent/room/database/AppDataBase.java
+++ app/src/main/java/org/flyve/mdm/agent/room/database/AppDataBase.java
@@ -1,37 +1,25 @@
-package husaynhakeem.io.androidroom_crud.database;
+package org.flyve.mdm.agent.room.database;
 
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import org.flyve.mdm.agent.room.dao.ApplicationDao;
+import org.flyve.mdm.agent.room.entity.Application;
 
-import husaynhakeem.io.androidroom_crud.dao.AddressDao;
-import husaynhakeem.io.androidroom_crud.dao.CatDao;
-import husaynhakeem.io.androidroom_crud.dao.PersonDao;
-import husaynhakeem.io.androidroom_crud.entity.Address;
-import husaynhakeem.io.androidroom_crud.entity.Cat;
-import husaynhakeem.io.androidroom_crud.entity.Person;
 
-/**
- * Created by husaynhakeem on 6/12/17.
- */
-
-@Database(entities = {Person.class, Address.class, Cat.class}, version = 1, exportSchema = false)
+@Database(entities = {Application.class}, version = 1, exportSchema = false)
 public abstract class AppDataBase extends RoomDatabase {
 
     private static AppDataBase instance;
 
-
-    public abstract CatDao catDao();
-    public abstract PersonDao personDao();
-    public abstract AddressDao addressDao();
-
+    public abstract ApplicationDao ApplicationDao();
 
     public static AppDataBase getAppDatabase(Context context) {
         if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     AppDataBase.class,
-                    "cat-owners-db")
+                    "flyve-mdm-db")
                     .allowMainThreadQueries()
                     .build();
         }
