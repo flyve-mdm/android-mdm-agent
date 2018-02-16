4c10480c6a4fa67951158b5854ee0356aa0a134b
Rafa Hernandez <rhernandez@teclib.com>
2018-02-16 09:11:19 -0400

    feat(app): implement install apk
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- app/src/main/java/org/flyve/mdm/agent/ui/InstallAppActivity.java
+++ app/src/main/java/org/flyve/mdm/agent/ui/InstallAppActivity.java
@@ -1,9 +1,16 @@
 package org.flyve.mdm.agent.ui;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import org.flyve.mdm.agent.R;
+import org.flyve.mdm.agent.utils.FlyveLog;
+
+import java.io.File;
 
 /*
  *   Copyright © 2018 Teclib. All rights reserved.
@@ -33,14 +40,99 @@ import org.flyve.mdm.agent.R;
  */
 public class InstallAppActivity extends Activity {
 
+    private static final int APP_INSTALL_REQUEST = 1010;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_install_app);
 
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String id = extras.getString("APP_ID");
+            String appPath = extras.getString("APP_PATH");
 
+            installApk(appPath);
+        } else {
+            finish();
+        }
 
+    }
+
+    /**
+     * Install the Android Package
+     * @param file to install
+     */
+    public void installApk(String file) {
+
+        FlyveLog.d(file);
+        Uri uri = Uri.fromFile(new File(file));
+        if (uri == null) {
+            throw new RuntimeException("Set the data uri to point to an apk location!");
+        }
+        // https://code.google.com/p/android/issues/detail?id=205827
+        if ((Build.VERSION.SDK_INT < 24)
+                && (!uri.getScheme().equals("file"))) {
+            throw new RuntimeException("PackageInstaller < Android N only supports file scheme!");
+        }
+        if ((Build.VERSION.SDK_INT >= 24)
+                && (!uri.getScheme().equals("content"))) {
+            throw new RuntimeException("PackageInstaller >= Android N only supports content scheme!");
+        }
+
+        Intent intent = new Intent();
+
+        // Note regarding EXTRA_NOT_UNKNOWN_SOURCE:
+        // works only when being installed as system-app
+        // https://code.google.com/p/android/issues/detail?id=42253
+
+        if (Build.VERSION.SDK_INT < 14) {
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setDataAndType(uri, "application/vnd.android.package-archive");
+        } else if (Build.VERSION.SDK_INT < 16) {
+            intent.setAction(Intent.ACTION_INSTALL_PACKAGE);
+            intent.setData(uri);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra(Intent.EXTRA_RETURN_RESULT, true);
+            intent.putExtra(Intent.EXTRA_NOT_UNKNOWN_SOURCE, true);
+            intent.putExtra(Intent.EXTRA_ALLOW_REPLACE, true);
+        } else if (Build.VERSION.SDK_INT < 24) {
+            intent.setAction(Intent.ACTION_INSTALL_PACKAGE);
+            intent.setData(uri);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra(Intent.EXTRA_RETURN_RESULT, true);
+            intent.putExtra(Intent.EXTRA_NOT_UNKNOWN_SOURCE, true);
+        } else { // Android N
+            intent.setAction(Intent.ACTION_INSTALL_PACKAGE);
+            intent.setData(uri);
+            // grant READ permission for this content Uri
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.putExtra(Intent.EXTRA_RETURN_RESULT, true);
+            intent.putExtra(Intent.EXTRA_NOT_UNKNOWN_SOURCE, true);
+        }
+
+        try {
+            startActivityForResult(intent, APP_INSTALL_REQUEST);
+        } catch (ActivityNotFoundException e) {
+            FlyveLog.e(e.getMessage());
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case APP_INSTALL_REQUEST:
+                if (resultCode == RESULT_OK) {
+                    FlyveLog.d("Package Installation Success");
+                } else if (resultCode == RESULT_FIRST_USER) {
+                    FlyveLog.e("Package Installation Cancelled by USER");
+                } else {
+                    FlyveLog.e("Something went wrong - INSTALLATION FAILED");
+                }
+        }
     }
 
 }
