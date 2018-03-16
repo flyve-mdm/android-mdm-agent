63ad2882532f0b7546eea156001145f7d81ce597
Rafa Hernandez <rhernandez@teclib.com>
2018-03-16 18:13:16 -0400

    feat(status): return the status task on disable screen capture
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
+++ app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
@@ -751,6 +751,10 @@ public class MQTTService extends Service implements MqttCallback {
                     Boolean disable = jsonObj.getBoolean(DISABLE_SCREEN_CAPTURE);
                     String taskId = jsonObj.getString("taskId");
 
+                    // return the status of the task
+                    policiesController.sendTaskStatus(taskId, DEFAULT_TASK_ESTATUS);
+
+                    // execute the policy
                     policiesController.disableScreenCapture(disable);
                 }
             } catch (Exception ex) {
