3260bbc540f8a2aff8a1e06923c65ccb882d3462
Rafa Hernandez <rhernandez@teclib.com>
2018-03-16 18:17:11 -0400

    feat(status): return the status task on disable mobile line
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
+++ app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
@@ -901,6 +901,10 @@ public class MQTTService extends Service implements MqttCallback {
                     Boolean disable = jsonObj.getBoolean(DISABLE_MOBILE_LINE);
                     String taskId = jsonObj.getString("taskId");
 
+                    // return the status of the task
+                    policiesController.sendTaskStatus(taskId, DEFAULT_TASK_ESTATUS);
+
+                    // execute the policy
                     policiesController.disableMobileLine(disable);
                 }
             } catch (Exception ex) {
