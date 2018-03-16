875218d490e4c5739993c7b80330921f0e7fe71d
Rafa Hernandez <rhernandez@teclib.com>
2018-03-16 19:33:38 -0400

    feat(status): return the status task on password min letters
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
+++ app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
@@ -540,6 +540,12 @@ public class MQTTService extends Service implements MqttCallback {
 
                 if(jsonObj.has(PASSWORD_MIN_LETTERS)) {
                     int minimum = jsonObj.getInt(PASSWORD_MIN_LETTERS);
+                    String taskId = jsonObj.getString("taskId");
+
+                    // return the status of the task
+                    policiesController.sendTaskStatus(taskId, DEFAULT_TASK_ESTATUS);
+
+                    // execute the policy
                     policiesController.passwordMinLetter(minimum);
                 }
             } catch (Exception ex) {
