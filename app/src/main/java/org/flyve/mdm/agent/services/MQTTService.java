953ede5de8390c06b3fdf3949df784a2d062ea69
Rafa Hernandez <rhernandez@teclib.com>
2018-03-16 19:32:55 -0400

    feat(status): return the status task on password min symbols
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
+++ app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
@@ -570,6 +570,12 @@ public class MQTTService extends Service implements MqttCallback {
 
                 if(jsonObj.has(PASSWORD_MIN_SYMBOLS)) {
                     int minimum = jsonObj.getInt(PASSWORD_MIN_SYMBOLS);
+                    String taskId = jsonObj.getString("taskId");
+
+                    // return the status of the task
+                    policiesController.sendTaskStatus(taskId, DEFAULT_TASK_ESTATUS);
+
+                    // execute the policy
                     policiesController.passwordMinSymbols(minimum);
                 }
             } catch (Exception ex) {
