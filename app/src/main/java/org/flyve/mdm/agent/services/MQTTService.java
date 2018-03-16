484e2b39ba2f1f2d341411dfaa92fb870f8ad9e1
Rafa Hernandez <rhernandez@teclib.com>
2018-03-16 18:14:12 -0400

    feat(status): return the status task on disable hostpot
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
+++ app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
@@ -816,6 +816,10 @@ public class MQTTService extends Service implements MqttCallback {
                     Boolean disable = jsonObj.getBoolean(DISABLE_HOSTPOT_TETHERING);
                     String taskId = jsonObj.getString("taskId");
 
+                    // return the status of the task
+                    policiesController.sendTaskStatus(taskId, DEFAULT_TASK_ESTATUS);
+
+                    // execute the policy
                     policiesController.disableHostpotTethering(disable);
                 }
             } catch (Exception ex) {
