9b976eb7ea2e8fcb5328b446548701541d04d7cb
Rafa Hernandez <rhernandez@teclib.com>
2018-03-16 18:52:18 -0400

    feat(status): return the status task on reset password
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
+++ app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
@@ -967,6 +967,10 @@ public class MQTTService extends Service implements MqttCallback {
                     Boolean disable = jsonObj.getBoolean(RESET_PASSWORD);
                     String taskId = jsonObj.getString("taskId");
 
+                    // return the status of the task
+                    policiesController.sendTaskStatus(taskId, DEFAULT_TASK_ESTATUS);
+
+                    // execute the policy
                     //policiesController.resetPassword(disable);
                 }
             } catch (Exception ex) {
