13a2b96190fa4f08b72a3abdd39d5d4261629856
Rafa Hernandez <rhernandez@teclib.com>
2018-03-16 18:16:06 -0400

    feat(status): return the status task on disable wifi
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
+++ app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
@@ -858,6 +858,10 @@ public class MQTTService extends Service implements MqttCallback {
                     Boolean disable = jsonObj.getBoolean(DISABLE_WIFI);
                     String taskId = jsonObj.getString("taskId");
 
+                    // return the status of the task
+                    policiesController.sendTaskStatus(taskId, DEFAULT_TASK_ESTATUS);
+
+                    // execute the policy
                     policiesController.disableWifi(disable);
                 }
             } catch (Exception ex) {
