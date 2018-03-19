922a1cc6c077bff839e6b2a129b1f1d858bd169f
Rafa Hernandez <rhernandez@teclib.com>
2018-03-19 16:45:28 -0400

    feat(permission): add permissions enrollment activity
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- app/src/main/java/org/flyve/mdm/agent/ui/PermissionEnrollmentActivity.java
+++ app/src/main/java/org/flyve/mdm/agent/ui/PermissionEnrollmentActivity.java
@@ -1,5 +1,24 @@
 package org.flyve.mdm.agent.ui;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import org.flyve.inventory.InventoryTask;
+import org.flyve.mdm.agent.R;
+import org.flyve.mdm.agent.core.enrollment.EnrollmentHelper;
+import org.flyve.mdm.agent.utils.Helpers;
+
 /*
  *   Copyright © 2018 Teclib. All rights reserved.
  *
@@ -18,7 +37,7 @@ package org.flyve.mdm.agent.ui;
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * ------------------------------------------------------------------------------
- * @author    rafaelhernandez
+ * @author    rafael hernandez
  * @date      19/3/18
  * @copyright Copyright © 2018 Teclib. All rights reserved.
  * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
@@ -26,5 +45,169 @@ package org.flyve.mdm.agent.ui;
  * @link      https://flyve-mdm.com
  * ------------------------------------------------------------------------------
  */
-public class PermissionEnrollmentActivity {
+public class PermissionEnrollmentActivity extends Activity {
+    private ProgressDialog progress;
+    private static final int REQUEST_EXIT = 1;
+    private LinearLayout lnButtons;
+    private Button btnPermission;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission_enrollment);
+
+        lnButtons = findViewById(R.id.lnButtons);
+
+        btnPermission = findViewById(R.id.btnPermission);
+        btnPermission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Build.VERSION.SDK_INT >= 23) {
+                    requestPermission();
+                } else {
+                    generateInventory();
+                }
+            }
+        });
+
+        Button btnShare = findViewById(R.id.btnShare);
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialogShare();
+            }
+        });
+
+        Button btnContinue = findViewById(R.id.btnContinue);
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent miIntent = new Intent(PermissionEnrollmentActivity.this, EnrollmentActivity.class);
+                PermissionEnrollmentActivity.this.startActivityForResult(miIntent, REQUEST_EXIT);
+            }
+        });
+    }
+
+    public void showDialogShare() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(PermissionEnrollmentActivity.this);
+        builder.setTitle(R.string.dialog_share_title);
+
+        final int[] type = new int[1];
+
+        //list of items
+        String[] items = getResources().getStringArray(R.array.export_list);
+        builder.setSingleChoiceItems(items, 0,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        type[0] = which;
+                    }
+                });
+
+        String positiveText = getString(android.R.string.ok);
+        builder.setPositiveButton(positiveText,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // positive button logic
+                        Helpers.share( PermissionEnrollmentActivity.this, "Inventory File", type[0] );
+                    }
+                });
+
+        String negativeText = getString(android.R.string.cancel);
+        builder.setNegativeButton(negativeText,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // negative button logic
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        // display dialog
+        dialog.show();
+    }
+
+    private void generateInventory() {
+        progress = ProgressDialog.show(this, "MDM Agent",
+                "Creating inventory...", true);
+
+        InventoryTask inventoryTask = new InventoryTask(PermissionEnrollmentActivity.this, "", true);
+        inventoryTask.getXML(new InventoryTask.OnTaskCompleted() {
+            @Override
+            public void onTaskSuccess(String s) {
+                progress.setMessage("Creating session...");
+
+                EnrollmentHelper sessionToken = new EnrollmentHelper(PermissionEnrollmentActivity.this);
+                sessionToken.getActiveSessionToken(new EnrollmentHelper.EnrollCallBack() {
+                    @Override
+                    public void onSuccess(String data) {
+                        // Active EnrollmentHelper Token is stored on cache
+                        progress.dismiss();
+
+                        lnButtons.setVisibility(View.VISIBLE);
+                        btnPermission.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        showError(error);
+                    }
+                });
+
+            }
+
+            @Override
+            public void onTaskError(Throwable throwable) {
+                showError("The inventory fail");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_EXIT && resultCode == RESULT_OK) {
+            this.finish();
+        }
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(PermissionEnrollmentActivity.this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_PHONE_STATE,
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.RECEIVE_SMS,
+                        Manifest.permission.READ_SMS,
+                },
+                1);
+    }
+
+    public void showError(String message) {
+        Helpers.snack(this, message, this.getResources().getString(R.string.snackbar_close), new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[2] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[3] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[4] == PackageManager.PERMISSION_GRANTED) {
+                    generateInventory();
+                } else {
+                    showError("All the permissions are required");
+                }
+            }
+        }
+    }
 }
