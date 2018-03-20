c862c28f620013d172307fb840eb8d221ad0497f
Rafa Hernandez <rhernandez@teclib.com>
2018-03-20 13:02:48 -0400

    refactor(permission): create permission interface
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

---
+++ app/src/main/java/org/flyve/mdm/agent/core/permission/Permission.java
@@ -0,0 +1,52 @@
+package org.flyve.mdm.agent.core.permission;
+
+import android.content.Context;
+
+/*
+ *   Copyright © 2018 Teclib. All rights reserved.
+ *
+ *   This file is part of flyve-mdm-android
+ *
+ * flyve-mdm-android is a subproject of Flyve MDM. Flyve MDM is a mobile
+ * device management software.
+ *
+ * Flyve MDM is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Flyve MDM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Rafael Hernandez
+ * @date      4/1/18
+ * @copyright Copyright © 2018 Teclib. All rights reserved.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/flyve-mdm-android
+ * @link      https://flyve-mdm.com
+ * ------------------------------------------------------------------------------
+ */
+public interface Permission {
+
+    interface View {
+        void showError(String message);
+        void inventorySuccess();
+    }
+
+    interface Presenter {
+        // Views
+        void showError(String message);
+        void inventorySuccess();
+
+        // Models
+        void showDialogShare(final Context context);
+        void generateInventory(final Context context);
+    }
+
+    interface Model {
+        void showDialogShare(final Context context);
+        void generateInventory(final Context context);
+    }
+}
