349a93739afa0e986008ba8e4fce13ebc4bfc949
Rafa Hernandez <rhernandez@teclib.com>
2018-03-22 16:42:16 -0400

    docs(data): update license to keep update and clean
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- app/src/main/java/org/flyve/mdm/agent/data/MqttData.java
+++ app/src/main/java/org/flyve/mdm/agent/data/MqttData.java
@@ -1,14 +1,7 @@
-package org.flyve.mdm.agent.data;
-
-import android.content.Context;
-
 /*
- *   Copyright © 2018 Teclib. All rights reserved.
+ * Copyright Teclib. All rights reserved.
  *
- *   This file is part of flyve-mdm-android
- *
- * flyve-mdm-android is a subproject of Flyve MDM. Flyve MDM is a mobile
- * device management software.
+ * Flyve MDM is a mobile device management software.
  *
  * Flyve MDM is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -20,14 +13,18 @@ import android.content.Context;
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * ------------------------------------------------------------------------------
- * @author    rafaelhernandez
- * @date      8/1/18
- * @copyright Copyright © 2018 Teclib. All rights reserved.
+ * @author    Rafael Hernandez
+ * @copyright Copyright Teclib. All rights reserved.
  * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
- * @link      https://github.com/flyve-mdm/flyve-mdm-android
+ * @link      https://github.com/flyve-mdm/android-mdm-agent
  * @link      https://flyve-mdm.com
  * ------------------------------------------------------------------------------
  */
+
+package org.flyve.mdm.agent.data;
+
+import android.content.Context;
+
 public class MqttData extends LocalStorage {
 
     /**
