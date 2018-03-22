e0e7dc4d4ab8022ed113744592086a6114035792
Rafa Hernandez <rhernandez@teclib.com>
2018-03-22 13:15:18 -0400

    feat(feedback): add feedback class
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- app/src/main/java/org/flyve/mdm/agent/ui/FragmentFeedback.java
+++ app/src/main/java/org/flyve/mdm/agent/ui/FragmentFeedback.java
@@ -1,15 +1,23 @@
 package org.flyve.mdm.agent.ui;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Switch;
 
 import org.flyve.mdm.agent.R;
 import org.flyve.mdm.agent.data.AppData;
+import org.flyve.mdm.agent.data.PoliciesData;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 /*
  *   Copyright © 2017 Teclib. All rights reserved.
@@ -37,7 +45,7 @@ import org.flyve.mdm.agent.data.AppData;
  * @link      https://flyve-mdm.com
  * ------------------------------------------------------------------------------
  */
-public class FragmentConfiguration extends Fragment {
+public class FragmentFeedback extends Fragment {
 
     private AppData cache;
 
@@ -46,29 +54,88 @@ public class FragmentConfiguration extends Fragment {
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_configuration, container, false);
+        View v = inflater.inflate(R.layout.fragment_feedback, container, false);
 
-        cache = new AppData(FragmentConfiguration.this.getContext());
+        HashMap<String, Boolean> map = new HashMap<>();
 
-        Switch swNotifications = v.findViewById(R.id.swNotifications);
-        swNotifications.setChecked(cache.getDisableNotification());
-        swNotifications.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        PoliciesData cache = new PoliciesData(FragmentFeedback.this.getContext());
+
+        map.put("Ping",false);
+        map.put("Geolocate",false);
+        map.put("Inventory",false);
+        map.put("Lock",false);
+        map.put("Wipe",false);
+        map.put("Unenroll",false);
+        map.put("Password enabled", false);
+        map.put("Password quality", !cache.getPasswordQuality().isEmpty() );
+        map.put("Password minimum length", !cache.getPasswordLength().isEmpty() );
+        map.put("Password minimum lower case", !cache.getPasswordMinimumLowerCase().isEmpty() );
+        map.put("Password minimum upper case", !cache.getPasswordMinimumUpperCase().isEmpty() );
+        map.put("Password minimum non letter",!cache.getPasswordMinimumNonLetter().isEmpty() );
+        map.put("Password minimum letters", !cache.getPasswordMinimumLetters().isEmpty() );
+        map.put("Password minimum numeric", !cache.getPasswordMinimumNumeric().isEmpty() );
+        map.put("Password minimum symbols", !cache.getPasswordMinimumSymbols().isEmpty() );
+        map.put("Maximum failed passwords for wipe", !cache.getMaximumFailedPasswordsForWipe().isEmpty() );
+        map.put("Maximum time to lock", !cache.getMaximumTimeToLock().isEmpty());
+        map.put("Storage encryption", !cache.getStorageEncryptionDevice());
+        map.put("Disable camera", !cache.getDisableCamera());
+        map.put("Disable bluetooth", !cache.getConnectivityBluetoothDisable());
+        map.put("Deploy app", false);
+        map.put("Remove app",false);
+        map.put("Deploy file",true);
+        map.put("Remove file",false);
+        map.put("Disable screen capture", false);
+        map.put("Disable airplane mode",!cache.getConnectivityAirplaneModeDisable());
+        map.put("Disable GPS", !cache.getConnectivityGPSDisable());
+        map.put("Disable Hostpot/Tethering", !cache.getConnectivityHostpotTetheringDisable());
+        map.put("Disable roaming", !cache.getConnectivityRoamingDisable());
+        map.put("Disable wifi", !cache.getConnectivityWifiDisable());
+        map.put("Use TLS", false);
+        map.put("Disable mobile line", !cache.getConnectivityMobileLineDisable());
+        map.put("Disable NFC", !cache.getConnectivityNFCDisable());
+        map.put("Disable statusbar", false);
+//        map.put("ResetPassword",false);
+        map.put("Disable Usb Mtp", !cache.getConnectivityMTPUsbFileTransferProtocolsDisable() );
+        map.put("Disable Usb Ptp", !cache.getConnectivityPTPUsbFileTransferProtocolsDisable() );
+        map.put("Disable Usb Adb", !cache.getConnectivityADBUsbFileTransferProtocolsDisable() );
+        map.put("Disable speakerphone", !cache.getdisableSpeakerphone());
+//        map.put("DisableSmsMms",false);
+        map.put("Disable create VPN Profiles", false);
+
+        LinearLayout ln = v.findViewById(R.id.lnFields);
+
+        Map<String, Boolean> treeMap = new TreeMap<>(map);
+        createField(FragmentFeedback.this.getContext(), treeMap, ln);
+
+        Button btnSend = v.findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                cache.setDisableNotification(b);
-            }
-        });
+            public void onClick(View view) {
 
-        Switch swDarkTheme = v.findViewById(R.id.swDarkTheme);
-        swDarkTheme.setChecked(cache.getDarkTheme());
-        swDarkTheme.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                cache.setDarkTheme(b);
             }
         });
 
         return v;
     }
 
+    private void createField(Context context, Map<String, Boolean> map, LinearLayout ln) {
+        Switch[] swPolicy = new Switch[map.size()];
+        int index = 0;
+
+        for(Map.Entry<String, Boolean> entry : map.entrySet()) {
+            String policy = entry.getKey();
+            Boolean highlight = entry.getValue();
+
+            swPolicy[index] = new Switch(context);
+            swPolicy[index].setText(policy);
+            if(highlight) {
+                swPolicy[index].setTextColor(Color.DKGRAY);
+            } else {
+                swPolicy[index].setTextColor(Color.GRAY);
+            }
+
+            ln.addView(swPolicy[index]);
+            index++;
+        }
     }
+}
