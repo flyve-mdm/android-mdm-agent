25dd99ddcde47f4cc54539b267a3c162039e8beb
Rafa Hernandez <rhernandez@teclib.com>
2018-03-26 14:04:26 -0400

    feat(feedback): send status for task id on remove package
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
+++ app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
@@ -732,11 +732,8 @@ public class MQTTService extends Service implements MqttCallback {
                     String removeApp = jsonObj.getString(REMOVE_APP);
                     String taskId = jsonObj.getString("taskId");
 
-                    // return the status of the task
-                    policiesController.sendTaskStatus(taskId, FEEDBACK_RECEIVED);
-
                     // execute the policy
-                    policiesController.removePackage(removeApp);
+                    policiesController.removePackage(taskId, removeApp);
                 }
             } catch (Exception ex) {
                 FlyveLog.e(ex.getMessage());
