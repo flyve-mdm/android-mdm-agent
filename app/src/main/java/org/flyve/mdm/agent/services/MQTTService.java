fea4780ac0eb439335dd0e07708b12974e0d0fe0
Rafa Hernandez <rhernandez@teclib.com>
2018-03-26 16:01:09 -0400

    feat(feedback): send status for task id on disable usb adb
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
+++ app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
@@ -1032,11 +1032,8 @@ public class MQTTService extends Service implements MqttCallback {
                     Boolean disable = jsonObj.getBoolean(DISABLE_USB_ADB);
                     String taskId = jsonObj.getString("taskId");
 
-                    // return the status of the task
-                    policiesController.sendTaskStatus(taskId, FEEDBACK_RECEIVED);
-
                     // execute the policy
-                    policiesController.disableADBUsbFileTransferProtocols(disable);
+                    policiesController.disableADBUsbFileTransferProtocols(taskId, disable);
                 }
             } catch (Exception ex) {
                 FlyveLog.e(ex.getMessage());
