b0bb40888126a820b1180f4a12db2684e3de6303
Rafa Hernandez <rhernandez@teclib.com>
2018-03-26 14:12:30 -0400

    feat(feedback): send status for task id on disable airplane mode
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
+++ app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
@@ -808,11 +808,8 @@ public class MQTTService extends Service implements MqttCallback {
                     Boolean disable = jsonObj.getBoolean(DISABLE_AIRPLANE_MODE);
                     String taskId = jsonObj.getString("taskId");
 
-                    // return the status of the task
-                    policiesController.sendTaskStatus(taskId, FEEDBACK_RECEIVED);
-
                     // execute the policy
-                    policiesController.disableAirplaneMode(disable);
+                    policiesController.disableAirplaneMode(taskId, disable);
                 }
             } catch (Exception ex) {
                 FlyveLog.e(ex.getMessage());
