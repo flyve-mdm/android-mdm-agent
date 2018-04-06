64bf8929c16a37f34d661386b42d1fee5fc1ba82
Rafa Hernandez <rhernandez@teclib.com>
2018-04-06 15:28:16 -0400

    feat(mqtt): remove setters
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- app/src/main/java/org/flyve/mdm/agent/data/MqttData.java
+++ app/src/main/java/org/flyve/mdm/agent/data/MqttData.java
@@ -30,8 +30,8 @@ import org.flyve.mdm.agent.room.entity.MQTT;
 
 public class MqttData {
 
-    AppDataBase dataBase;
-    MQTT mqtt;
+    private AppDataBase dataBase;
+    private MQTT mqtt;
     /**
      * Constructor
      *
@@ -39,294 +39,163 @@ public class MqttData {
      */
     public MqttData(Context context) {
         dataBase = AppDataBase.getAppDatabase(context);
-        mqtt = dataBase.MQTTDao().loadAll().get(0);
+        if(!dataBase.MQTTDao().loadAll().isEmpty()) {
+            mqtt = dataBase.MQTTDao().loadAll().get(0);
+        }
     }
 
+    public boolean isEmpty() {
+        return (mqtt==null);
+    }
+
+    private String getValue(String value) {
+        if(value!=null) {
+            return value;
+        } else {
+            return "";
+        }
+    }
     /**
      * Get the version of the Manifest
      * @return string the Manifest version
      */
     public String getManifestVersion() {
-        return mqtt.manifestVersion;
+        return getValue(mqtt.manifestVersion);
     }
-
-//    /**
-//     * Set the version of the Manifest
-//     * @param version number of the version
-//     */
-//    public void setManifestVersion(String version) {
-//        setData("manifestVersion", version);
-//    }
 
     /**
      * Get the URL
      * @return string the URL
      */
     public String getUrl() {
-        return mqtt.url;
+        return getValue(mqtt.url);
     }
-
-//    /**
-//     * Set the URL
-//     * @param url
-//     */
-//    public void setUrl(String url) {
-//        setData("url", url);
-//    }
 
     /**
      * Get the token of the user
      * @return string the user token
      */
     public String getUserToken() {
-        return mqtt.userToken;
+        return getValue(mqtt.userToken);
     }
-
-//    /**
-//     * Set the token of the user
-//     * @param userToken
-//     */
-//    public void setUserToken(String userToken) {
-//        setData("user_token", userToken);
-//    }
 
     /**
      * Get the invitation token
      * @return string the invitation token
      */
     public String getInvitationToken() {
-        return mqtt.invitationToken;
+        return getValue(mqtt.invitationToken);
     }
-
-//    /**
-//     * Set the invitation token
-//     * @param invitationToken
-//     */
-//    public void setInvitationToken(String invitationToken) {
-//        setData("invitation_token", invitationToken);
-//    }
 
     /**
      * Get the session token
      * @return string the session token
      */
     public String getSessionToken() {
-        return mqtt.sessionToken;
+        return getValue(mqtt.sessionToken);
     }
-
-//    /**
-//     * Set the session token
-//     * @param sessionToken
-//     */
-//    public void setSessionToken(String sessionToken) {
-//        setData("session_token", sessionToken);
-//    }
 
     /**
      * Get the ID of the profile
      * @return string the profile ID
      */
     public String getProfileId() {
-        return mqtt.profileId;
+        return getValue(mqtt.profileId);
     }
-
-//    /**
-//     * Set the ID of the profile
-//     * @param profileId
-//     */
-//    public void setProfileId(String profileId) {
-//        setData("profile_id", profileId);
-//    }
 
     /**
      * Get the ID of the agent
      * @return string the agent ID
      */
     public String getAgentId() {
-        return mqtt.agentId;
+        return getValue(mqtt.agentId);
     }
-
-//    /**
-//     * Set the ID of the agent
-//     * @param agentId
-//     */
-//    public void setAgentId(String agentId) {
-//        setData("agent_id", agentId);
-//    }
 
     /**
      * Get the broker
      * @return string the broker
      */
     public String getBroker() {
-        return mqtt.broker;
+        return getValue(mqtt.broker);
     }
-
-//    /**
-//     * Set the broker
-//     * @param broker
-//     */
-//    public void setBroker(String broker) {
-//        setData("broker", broker);
-//    }
 
     /**
      * Get the port
      * @return string the port
      */
     public String getPort() {
-        return mqtt.port;
+        return getValue(mqtt.port);
     }
-
-    /**
-//     * Set the port
-//     * @param port
-//     */
-//    public void setPort(String port) {
-//        setData("port", port);
-//    }
 
     /**
      * Get the Transport Layer Security (TLS)
      * @return string the TLS
      */
     public String getTls() {
-        return mqtt.tls;
+        return getValue(mqtt.tls);
     }
-
-//    /**
-//     * Set the Transport Layer Security (TLS)
-//     * @param tls
-//     */
-//    public void setTls(String tls) {
-//        setData("tls", tls);
-//    }
 
     /**
      * Get the topic
      * @return string the topic
      */
     public String getTopic() {
-        return mqtt.topic;
+        return getValue(mqtt.topic);
     }
-
-//    /**
-//     * Set the topic
-//     * @param topic
-//     */
-//    public void setTopic(String topic) {
-//        setData("topic", topic);
-//    }
 
     /**
      * Get the user of the Message Queue Telemetry Transport (MQTT)
      * @return string the MQTT user
      */
     public String getMqttuser() {
-        return mqtt.mqttuser;
+        return getValue(mqtt.mqttuser);
     }
-
-//    /**
-//     * Set the user of the Message Queue Telemetry Transport (MQTT)
-//     * @param mqttuser
-//     */
-//    public void setMqttuser(String mqttuser) {
-//        setData("mqttuser", mqttuser);
-//    }
 
     /**
      * Get the password of the Message Queue Telemetry Transport (MQTT)
      * @return the MQTT password
      */
     public String getMqttpasswd() {
-        return mqtt.mqttpasswd;
+        return getValue(mqtt.mqttpasswd);
     }
-
-//    /**
-//     * Set the password of the Message Queue Telemetry Transport (MQTT)
-//     * @param mqttpasswd
-//     */
-//    public void setMqttpasswd(String mqttpasswd) {
-//        setData("mqttpasswd", mqttpasswd);
-//    }
 
     /**
      * Get the Certificate
      * @return string the certificate
      */
     public String getCertificate() {
-        return mqtt.certificate;
+        return getValue(mqtt.certificate);
     }
-
-//    /**
-//     * Set the Certificate
-//     * @param certificate
-//     */
-//    public void setCertificate(String certificate) {
-//        setData("certificate", certificate);
-//    }
 
     /**
      * Get the name
      * @return string the name
      */
     public String getName() {
-        return mqtt.name;
+        return getValue(mqtt.name);
     }
-
-//    /**
-//     * Set the name
-//     * @param name
-//     */
-//    public void setName(String name) {
-//        setData("name", name);
-//    }
 
     /**
      * Get the ID of the computer
      * @return string the computer ID
      */
     public String getComputersId() {
-        return mqtt.computersId;
+        return getValue(mqtt.computersId);
     }
-
-//    /**
-//     * Set the ID of the computer
-//     * @param computersId
-//     */
-//    public void setComputersId(String computersId) {
-//        setData("computers_id", computersId);
-//    }
 
     /**
      * Get the ID of the entities
      * @return string the entities ID
      */
     public String getEntitiesId() {
-        return mqtt.entitiesId;
+        return getValue(mqtt.entitiesId);
     }
-
-//    /**
-//     * Set the ID of the entities
-//     * @param entitiesId
-//     */
-//    public void setEntitiesId(String entitiesId) {
-//        setData("entities_id", entitiesId);
-//    }
 
     /**
      * Get the ID of the Fleets of the Flyve MDM plugin
      * @return string the Fleets ID
      */
     public String getPluginFlyvemdmFleetsId() {
-        return mqtt.pluginFlyvemdmFleetsId;
+        return getValue(mqtt.pluginFlyvemdmFleetsId);
     }
-
-//    /**
-//     * Set the ID of the Fleets of the Flyve MDM plugin
-//     * @param pluginFlyvemdmFleetsId
-//     */
-//    public void setPluginFlyvemdmFleetsId(String pluginFlyvemdmFleetsId) {
-//        setData("plugin_flyvemdm_fleets_id", pluginFlyvemdmFleetsId);
-//    }
 }
