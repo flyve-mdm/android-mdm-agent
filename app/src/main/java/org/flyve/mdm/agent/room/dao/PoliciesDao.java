d3e059082866caad218410ee4f1d5fc7129c7ac7
Rafa Hernandez <rhernandez@teclib.com>
2018-04-08 10:34:04 -0400

    feat(policies): add database policies schema
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

---
+++ app/src/main/java/org/flyve/mdm/agent/room/dao/PoliciesDao.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright Teclib. All rights reserved.
+ *
+ * Flyve MDM is a mobile device management software.
+ *
+ * Flyve MDM is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Flyve MDM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Rafael Hernandez
+ * @copyright Copyright Teclib. All rights reserved.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/android-mdm-agent
+ * @link      https://flyve-mdm.com
+ * ------------------------------------------------------------------------------
+ */
+
+package org.flyve.mdm.agent.room.dao;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import org.flyve.mdm.agent.room.entity.Policies;
+
+import java.util.List;
+
+@Dao
+public interface PoliciesDao {
+
+    @Insert
+    void insert(Policies... policies);
+
+    @Update
+    void update(Policies... policies);
+
+    @Delete
+    void delete(Policies... policies);
+
+    @Query("Select * FROM policies")
+    List<Policies> loadAll();
+
+}
