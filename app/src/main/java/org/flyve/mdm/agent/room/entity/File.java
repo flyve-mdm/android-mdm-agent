d2650e919da1d8bb45dd48fee9baf92866781104
Rafa Hernandez <rhernandez@teclib.com>
2018-04-09 18:32:30 -0400

    feat(files): add database file structure
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

---
+++ app/src/main/java/org/flyve/mdm/agent/room/entity/File.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright Teclib. All rights reserved.
+ *
+ * Flyve MDM is a mobile device management software.
+ *
+ * Flyve MDM is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Flyve MDM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Rafael Hernandez
+ * @copyright Copyright Teclib. All rights reserved.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/android-mdm-agent
+ * @link      https://flyve-mdm.com
+ * ------------------------------------------------------------------------------
+ */
+
+package org.flyve.mdm.agent.room.entity;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity (tableName = "files")
+public class File {
+
+    @PrimaryKey (autoGenerate = true)
+    public int id;
+
+    @ColumnInfo (name = "file_id")
+    public String fileId;
+
+    @ColumnInfo (name = "file_name")
+    public String fileName;
+
+    @ColumnInfo (name = "file_path")
+    public String filePath;
+
+    @ColumnInfo (name = "file_status")
+    public String fileStatus;
+}
