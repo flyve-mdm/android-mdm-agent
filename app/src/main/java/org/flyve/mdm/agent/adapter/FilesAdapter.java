e6cfd2be136a807d277e7ebf59107ccab1812640
Rafa Hernandez <rhernandez@teclib.com>
2018-04-11 13:19:23 -0400

    feat(files): add files screen
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

---
+++ app/src/main/java/org/flyve/mdm/agent/adapter/FilesAdapter.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright Teclib. All rights reserved.
+ *
+ * Flyve MDM is a mobile device management software.
+ *
+ * Flyve MDM is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Flyve MDM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Rafael Hernandez
+ * @copyright Copyright Teclib. All rights reserved.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/android-mdm-agent
+ * @link      https://flyve-mdm.com
+ * ------------------------------------------------------------------------------
+ */
+
+package org.flyve.mdm.agent.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import org.flyve.mdm.agent.R;
+import org.flyve.mdm.agent.room.entity.File;
+import org.flyve.mdm.agent.utils.FlyveLog;
+
+public class FilesAdapter extends BaseAdapter {
+
+	private File[] data;
+	private LayoutInflater inflater = null;
+
+	public FilesAdapter(Activity activity, File[] data) {
+		FlyveLog.d(activity.getLocalClassName());
+
+		this.data = data;
+		inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}
+
+	/**
+	 * Get how many items are in the data
+	 * @return int the data size
+	 */
+	@Override
+	public int getCount() {
+		return data.length;
+	}
+
+	/**
+	 * Get the data item associated with the specified position
+     * @param position of the item whose data we want
+	 * @return Object the data at the specified position
+ 	 */
+	@Override
+	public Object getItem(int position) {
+		return position;
+	}
+
+	/**
+	 * Get the row ID associated with the specified position
+	 * @param position of the item whose row ID we want
+	 * @return long the ID of the item at the specified position
+	 */
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	/**
+	 * Get a View that displays the data at the specified position
+	 * @param position of the item within the adapter's data set of the item whose View we want
+	 * @param convertView the old View to reuse, if possible
+	 * @param parent the parent that this View will eventually be attached to
+	 * @return View a View corresponding to the data at the specified position
+	 */
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+
+		File file = data[position];
+
+		View vi = inflater.inflate(R.layout.list_item_file, null);
+
+		TextView txtFileName = vi.findViewById(R.id.txtFileName);
+		txtFileName.setText(file.fileName);
+
+		TextView txtPath = vi.findViewById(R.id.txtPath);
+		txtPath.setText(file.filePath);
+
+		return vi;
+
+	}
+
+}
