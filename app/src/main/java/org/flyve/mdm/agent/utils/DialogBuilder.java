ad33b481f66b80321d88afebc19e3fe1723c5752
Rafa Hernandez <rhernandez@teclib.com>
2018-04-23 18:37:15 -0400

    feat(scan): add custom dialog builder
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

---
+++ app/src/main/java/org/flyve/mdm/agent/utils/DialogBuilder.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2014-2015 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.flyve.mdm.agent.utils;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.flyve.mdm.agent.R;
+
+public class DialogBuilder extends AlertDialog.Builder
+{
+	private final View customTitle;
+	private final ImageView iconView;
+	private final TextView titleView;
+
+	public static DialogBuilder warn(final Context context, final int titleResId)
+	{
+		final DialogBuilder builder = new DialogBuilder(context);
+		builder.setIcon(R.drawable.ic_warning_grey600_24dp);
+		builder.setTitle(titleResId);
+		return builder;
+	}
+
+	public DialogBuilder(final Context context)
+	{
+		super(context, Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP ? AlertDialog.THEME_HOLO_LIGHT : AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
+
+		this.customTitle = LayoutInflater.from(context).inflate(R.layout.dialog_title, null);
+		this.iconView = (ImageView) customTitle.findViewById(android.R.id.icon);
+		this.titleView = (TextView) customTitle.findViewById(android.R.id.title);
+	}
+
+	@Override
+	public DialogBuilder setIcon(final Drawable icon)
+	{
+		if (icon != null)
+		{
+			setCustomTitle(customTitle);
+			iconView.setImageDrawable(icon);
+			iconView.setVisibility(View.VISIBLE);
+		}
+
+		return this;
+	}
+
+	@Override
+	public DialogBuilder setIcon(final int iconResId)
+	{
+		if (iconResId != 0)
+		{
+			setCustomTitle(customTitle);
+			iconView.setImageResource(iconResId);
+			iconView.setVisibility(View.VISIBLE);
+		}
+
+		return this;
+	}
+
+	@Override
+	public DialogBuilder setTitle(final CharSequence title)
+	{
+		if (title != null)
+		{
+			setCustomTitle(customTitle);
+			titleView.setText(title);
+		}
+
+		return this;
+	}
+
+	@Override
+	public DialogBuilder setTitle(final int titleResId)
+	{
+		if (titleResId != 0)
+		{
+			setCustomTitle(customTitle);
+			titleView.setText(titleResId);
+		}
+
+		return this;
+	}
+
+	@Override
+	public DialogBuilder setMessage(final CharSequence message)
+	{
+		super.setMessage(message);
+
+		return this;
+	}
+
+	@Override
+	public DialogBuilder setMessage(final int messageResId)
+	{
+		super.setMessage(messageResId);
+
+		return this;
+	}
+
+	public DialogBuilder singleDismissButton(@Nullable final DialogInterface.OnClickListener dismissListener)
+	{
+		setNeutralButton(R.string.button_dismiss, dismissListener);
+
+		return this;
+	}
+}
