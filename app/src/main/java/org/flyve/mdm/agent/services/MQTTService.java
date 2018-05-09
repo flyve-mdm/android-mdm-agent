4be840399e9b088f8dc9206dbb549d97f16c0017
Rafa Hernandez <rhernandez@teclib.com>
2018-05-09 17:29:49 -0400

    feat(policy): implement disable stream voicecall policy
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
+++ app/src/main/java/org/flyve/mdm/agent/services/MQTTService.java
@@ -1253,6 +1253,23 @@ public class MQTTService extends Service implements MqttCallback {
             }
         }
 
+        String DISABLE_STREAM_VOICECALL = "disableStreamVoiceCall";
+        if(topic.toLowerCase().contains(DISABLE_STREAM_VOICECALL.toLowerCase())) {
+            try {
+                JSONObject jsonObj = new JSONObject(messageBody);
+
+                if(jsonObj.has(DISABLE_STREAM_VOICECALL)) {
+                    Boolean disable = jsonObj.getBoolean(DISABLE_STREAM_VOICECALL);
+                    String taskId = jsonObj.getString("taskId");
+
+                    // execute the policy
+                    policiesController.disableSounds(AudioManager.STREAM_VOICE_CALL, taskId, disable);
+                }
+            } catch (Exception ex) {
+                FlyveLog.e(ex.getMessage());
+            }
+        }
+
     }
 
 
