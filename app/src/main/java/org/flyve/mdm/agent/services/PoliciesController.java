bfa271df105e7f3a2a0d40ae1b2bbbd961d6e93b
Rafa Hernandez <rhernandez@teclib.com>
2018-05-17 10:01:42 -0400

    feat(policy): add password min length policy pattern
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- app/src/main/java/org/flyve/mdm/agent/services/PoliciesController.java
+++ app/src/main/java/org/flyve/mdm/agent/services/PoliciesController.java
@@ -399,31 +399,6 @@ public class PoliciesController {
         }
     }
 
-    public void passwordMinLength(String taskId, int length, int priority) {
-        try {
-            // Set on database and get priority value
-            Object priorityValue = cache.setPasswordMinimumLength(length, priority);
-
-            // check Priority
-            if(priorityValue!=null) {
-                length = Integer.parseInt(priorityValue.toString());
-            }
-
-            // Execute the policy
-            PoliciesDeviceManager mdm = new PoliciesDeviceManager(this.context);
-            mdm.setPasswordLength(length);
-            broadcastReceivedLog(Helpers.broadCastMessage(MQTT_SEND, "passwordMinLength", String.valueOf(length)));
-
-            // return the status of the task
-            sendTaskStatus(taskId, FEEDBACK_DONE);
-        } catch (Exception ex) {
-            FlyveLog.e(ex.getMessage());
-
-            // return the status of the task
-            sendTaskStatus(taskId, FEEDBACK_FAILED);
-        }
-    }
-
     public void passwordMinNonLetter(String taskId, int length, int priority) {
         try {
             // Set on database and get priority value
