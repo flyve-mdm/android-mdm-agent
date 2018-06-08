93c05084694366890703fa87f58efd199170c5d2
Naylin Medina <nmedina@teclib.com>
2018-06-08 18:53:37 -0400

    ci(circle): add update for properties values
    Signed-off-by: Naylin Medina <nmedina@teclib.com>

--- ci/scripts/ci_about.sh
+++ ci/scripts/ci_about.sh
@@ -8,3 +8,8 @@ echo "about.date=$(date "+%a %b %d %H:%M:%S %Y")" >> app/src/main/assets/about.p
 echo "about.commit=${CIRCLE_SHA1:0:7}" >> app/src/main/assets/about.properties
 echo "about.commitFull=$CIRCLE_SHA1" >> app/src/main/assets/about.properties
 echo "about.github=https://github.com/flyve-mdm/flyve-mdm-android-agent" >> app/src/main/assets/about.properties
+
+if [[ $CIRCLE_BRANCH == *"master"* || $CIRCLE_BRANCH == *"develop"* ]]; then
+    git add app/src/main/assets/about.properties
+    git commit -m "build(properties): add new properties values"
+fi
--- ci/scripts/ci_validate_workflow.sh
+++ ci/scripts/ci_validate_workflow.sh
@@ -2,7 +2,7 @@
 
 GH_COMMIT_MESSAGE=$(git log --pretty=oneline -n 1 $CIRCLE_SHA1)
 
-if [[ $GH_COMMIT_MESSAGE == *"ci(release): generate CHANGELOG.md for version"* || $GH_COMMIT_MESSAGE == *"ci(release): update version on android manifest"* ]]; then
+if [[ $GH_COMMIT_MESSAGE == *"ci(release): generate CHANGELOG.md for version"* || $GH_COMMIT_MESSAGE == *"build(properties): add new properties values"* || $GH_COMMIT_MESSAGE == *"ci(release): update version on android manifest"* ]]; then
     echo "Running duplicated"
     exit 1
 fi
