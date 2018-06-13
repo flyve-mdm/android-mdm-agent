10bd27896e0224689326d41e2f1c8d0a0687dffe
Naylin Medina <nmedina@teclib.com>
2018-06-13 10:55:32 -0400

    docs(headers): update license information
    Signed-off-by: Naylin Medina <nmedina@teclib.com>

--- ci/scripts/ci_javadoc.sh
+++ ci/scripts/ci_javadoc.sh
@@ -1,30 +1,31 @@
 #!/usr/bin/env bash
 #
-#  Copyright (C) 2017 Teclib'
+#  LICENSE
 #
-#  This file is part of Flyve MDM Inventory Agent Android.
+#  This file is part of Flyve MDM Agent for Android.
 #
-#  Flyve MDM Inventory Agent Android is a subproject of Flyve MDM. Flyve MDM is a mobile
+#  Flyve MDM Agent for Android is a subproject of Flyve MDM. Flyve MDM is a mobile
 #  device management software.
 #
-#  Flyve MDM Android is free software: you can redistribute it and/or
+#  Flyve MDM is free software: you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
 #  as published by the Free Software Foundation; either version 3
 #  of the License, or (at your option) any later version.
 #
-#  Flyve MDM Inventory Agent Android is distributed in the hope that it will be useful,
+#  Flyve MDM Agent for Android is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  ------------------------------------------------------------------------------
-#  @author    Rafael Hernandez - rafaelje
-#  @copyright Copyright (c) 2017 Flyve MDM
+#  --------------------------------------------------------------------------------
+#  @author    Rafael Hernandez - <rhernandez@teclib.com>
+#  @copyright Copyright (c) 2017 - 2018 Teclib'
 #  @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
-#  @link      https://github.com/flyve-mdm/flyve-mdm-android-inventory-agent/
-#  @link      http://www.glpi-project.org/
+#  @link      https://github.com/flyve-mdm/android-mdm-agent/
+#  @link      http://flyve.org/android-mdm-agent/
 #  @link      https://flyve-mdm.com/
-#  ------------------------------------------------------------------------------
+#  --------------------------------------------------------------------------------
 #
+
 # Generate code-documentation directory, this folder must be on .gitignore
 javadoc -d ./development/code-documentation -sourcepath ./app/src/main/java -subpackages . -bootclasspath $ANDROID_HOME/platforms/android-26/android.jar
 
--- ci/scripts/ci_validate_workflow.sh
+++ ci/scripts/ci_validate_workflow.sh
@@ -1,4 +1,30 @@
 #!/usr/bin/env bash
+#
+#  LICENSE
+#
+#  This file is part of Flyve MDM Agent for Android.
+#
+#  Flyve MDM Agent for Android is a subproject of Flyve MDM. Flyve MDM is a mobile
+#  device management software.
+#
+#  Flyve MDM is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 3
+#  of the License, or (at your option) any later version.
+#
+#  Flyve MDM Agent for Android is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  --------------------------------------------------------------------------------
+#  @author    Rafael Hernandez - <rhernandez@teclib.com>
+#  @copyright Copyright (c) 2017 - 2018 Teclib'
+#  @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+#  @link      https://github.com/flyve-mdm/android-mdm-agent/
+#  @link      http://flyve.org/android-mdm-agent/
+#  @link      https://flyve-mdm.com/
+#  --------------------------------------------------------------------------------
+#
 
 GH_COMMIT_MESSAGE=$(git log --pretty=oneline -n 1 $CIRCLE_SHA1)
 
