63591dcef8448ab3192f3e728a444f936d4ee5b9
Naylin Medina <nmedina@teclib.com>
2018-06-27 13:18:01 -0400

    ci(circle): create about commit on master
    add code documentation
    
    Signed-off-by: Naylin Medina <nmedina@teclib.com>

--- ci/scripts/ci_about.sh
+++ ci/scripts/ci_about.sh
@@ -35,7 +35,8 @@ echo "about.commit=${CIRCLE_SHA1:0:7}" >> app/src/main/assets/about.properties
 echo "about.commitFull=$CIRCLE_SHA1" >> app/src/main/assets/about.properties
 echo "about.github=https://github.com/flyve-mdm/flyve-mdm-android-agent" >> app/src/main/assets/about.properties
 
-if [[ $CIRCLE_BRANCH == *"master"* || $CIRCLE_BRANCH == *"develop"* ]]; then
+if [[ $CIRCLE_BRANCH == *"master"* ]]; then
     git add app/src/main/assets/about.properties
     git commit -m "build(properties): add new properties values"
+    git push origin $CIRCLE_BRANCH
 fi
