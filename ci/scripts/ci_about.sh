35ee6ed9534a86ff48ecdf263679a01ccc4ff126
Naylin Medina <nmedina@teclib.com>
2018-07-03 10:23:10 -0400

    ci(circle): update about script
    commit changes
    
    Signed-off-by: Naylin Medina <nmedina@teclib.com>

--- ci/scripts/ci_about.sh
+++ ci/scripts/ci_about.sh
@@ -36,8 +36,10 @@ echo "about.commit=${CIRCLE_SHA1:0:7}" >> app/src/main/assets/about.properties
 echo "about.commitFull=$CIRCLE_SHA1" >> app/src/main/assets/about.properties
 echo "about.github=https://github.com/flyve-mdm/flyve-mdm-android-agent" >> app/src/main/assets/about.properties
 
+git add app/src/main/assets/about.properties
+git commit -m "build(properties): add new properties values"
+
+# push changes only in master branch
 if [[ $CIRCLE_BRANCH == *"master"* ]]; then
-    git add app/src/main/assets/about.properties
-    git commit -m "build(properties): add new properties values"
     git push origin $CIRCLE_BRANCH
 fi
