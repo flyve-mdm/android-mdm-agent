5c83c90fef5335dd3f750a602b23f7300f95e511
Naylin Medina <nmedina@teclib.com>
2018-07-24 10:39:43 -0400

    ci(circle): migrate gh-pages to circle
    remove .travis.yml file
    add circle config to yamllint
    
    Signed-off-by: Naylin Medina <nmedina@teclib.com>

--- ci/yamllint.sh
+++ ci/yamllint.sh
@@ -1,4 +1,4 @@
 ./node_modules/.bin/yamllint _config.yml
 ./node_modules/.bin/yamllint .eslintrc.yml
 ./node_modules/.bin/yamllint .sass-lint.yml
-./node_modules/.bin/yamllint .travis.yml
+./node_modules/.bin/yamllint .circleci/config.yml
