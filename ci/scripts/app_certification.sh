968e0d07ff30fbac3b70dbfd646f03103dbd7efd
Rafa Hernandez <rhernandez@teclib.com>
2018-07-26 00:06:11 -0400

    ci(certification): add scripts

--- ci/scripts/app_certification.sh
+++ ci/scripts/app_certification.sh
@@ -1,1 +1,38 @@
+#!/usr/bin/env bash
+#
+#  LICENSE
+#
+#  This file is part of Flyve MDM Agent for Android.
+#
+#  Flyve MDM Agent for Android is a subproject of Flyve MDM. Flyve MDM is a mobile
+#  device management software.
+#
+#  Flyve MDM is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 3
+#  of the License, or (at your option) any later version.
+#
+#  Flyve MDM Agent for Android is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  --------------------------------------------------------------------------------
+#  @author    Rafael Hernandez - <rhernandez@teclib.com>
+#  @copyright Copyright (c) Teclib'
+#  @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+#  @link      https://github.com/flyve-mdm/android-mdm-agent/
+#  @link      http://flyve.org/android-mdm-agent/
+#  @link      https://flyve-mdm.com/
+#  --------------------------------------------------------------------------------
+#
+
+# install all needed
+./ci/scripts/setup_enviroment.sh
+
+# run about script
+./ci/scripts/create_about_data.sh
+
+# run transifex script
+./ci/scripts/transifex.sh
+
 fastlane android certification storepass:'$KEYSTORE' keypass:'$ALIAS'
