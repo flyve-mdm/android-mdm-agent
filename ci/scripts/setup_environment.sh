23d269b7a00ec7c5744c306c8739f0adcaef6285
Rafa Hernandez <rhernandez@teclib.com>
2018-07-27 19:59:42 -0400

    ci(setup): add git tag
    - git tag version number
    - unify create apk and reorder
    - add validate running script
    - remove commits
    - unify create apk and release
    
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- ci/scripts/setup_environment.sh
+++ ci/scripts/setup_environment.sh
@@ -53,3 +53,6 @@ git config --global user.email $GITHUB_EMAIL
 git config --global user.name "Teclib"
 git remote remove origin
 git remote add origin https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git
+
+# Get version number from package.json
+export GIT_TAG=$(jq -r ".version" package.json)
