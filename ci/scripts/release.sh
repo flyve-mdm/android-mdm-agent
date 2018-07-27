65241d77d05087b443dab87f0645ead95dfb7f7d
Rafa Hernandez <rhernandez@teclib.com>
2018-07-27 19:59:42 -0400

    ci(setup): add git tag
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- ci/scripts/release.sh
+++ ci/scripts/release.sh
@@ -41,9 +41,6 @@ else
 
 fi
 
-# Get version number from package.json
-export GIT_TAG=$(jq -r ".version" package.json)
-
 # send changelog to gh-pages
 yarn gh-pages --dist ./ --src CHANGELOG.md --dest ./_includes/ --add -m "docs(changelog): update changelog with version ${GIT_TAG}"
 
