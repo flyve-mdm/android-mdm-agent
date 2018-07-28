9384a3db925ca075caecf53753d85ad9e8414856
Rafa Hernandez <rhernandez@teclib.com>
2018-07-27 20:02:47 -0400

    ci(about): remove commits
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- ci/scripts/create_about_data.sh
+++ ci/scripts/create_about_data.sh
@@ -27,19 +27,9 @@
 #  --------------------------------------------------------------------------------
 #
 
-GIT_TAG=$(jq -r ".version" package.json)
-
 echo "about.version=${GIT_TAG}" > app/src/main/assets/about.properties
 echo "about.build=$CIRCLE_BUILD_NUM" >> app/src/main/assets/about.properties
 echo "about.date=$(date "+%a %b %d %H:%M:%S %Y")" >> app/src/main/assets/about.properties
 echo "about.commit=${CIRCLE_SHA1:0:7}" >> app/src/main/assets/about.properties
 echo "about.commitFull=$CIRCLE_SHA1" >> app/src/main/assets/about.properties
 echo "about.github=https://github.com/flyve-mdm/flyve-mdm-android-agent" >> app/src/main/assets/about.properties
-
-git add app/src/main/assets/about.properties
-git commit -m "build(properties): add new properties values"
-
-# push changes only in master branch
-if [[ $CIRCLE_BRANCH == *"master"* ]]; then
-    git push origin $CIRCLE_BRANCH
-fi
