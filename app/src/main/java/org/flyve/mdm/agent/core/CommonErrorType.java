f2a61e5e8860d68834ccde92c5aa19f03f82fb92
Rafa Hernandez <rhernandez@teclib.com>
2018-08-21 15:46:15 -0400

    feat(error): create common error type class
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

---
+++ app/src/main/java/org/flyve/mdm/agent/core/CommonErrorType.java
@@ -0,0 +1,41 @@
+package org.flyve.mdm.agent.core;
+
+/*
+ *   Copyright © 2018 Teclib. All rights reserved.
+ *
+ *   This file is part of flyve-mdm-android
+ *
+ * flyve-mdm-android is a subproject of Flyve MDM. Flyve MDM is a mobile
+ * device management software.
+ *
+ * Flyve MDM is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Flyve MDM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Rafael Hernandez
+ * @date      21/8/18
+ * @copyright Copyright © 2018 Teclib. All rights reserved.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/flyve-mdm-android
+ * @link      https://flyve-mdm.com
+ * ------------------------------------------------------------------------------
+ */
+
+public class CommonErrorType {
+
+    public static final int DEEPLINK_BASE64DECODE = 100;
+    public static final int DEEPLINK_URL_EMPTY = 101;
+    public static final int DEEPLINK_USER_TOKEN = 102;
+    public static final int DEEPLINK_INVITATION_TOKEN = 103;
+    public static final int DEEPLINK_CSV_WRONG_FORMAT = 104;
+    public static final int DEEPLINK_GENERAL_EXCEPTION = 105;
+    public static final int DEEPLINK_ENROLLMENT_FAIL = 106;
+    public static final int DEEPLINK_GETQUERYPARAMETER = 107;
+
+}
