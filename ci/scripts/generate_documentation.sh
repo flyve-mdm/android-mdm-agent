c1856533fb5386bfc6df57f821a8ad2a4a382d3a
Rafa Hernandez <rhernandez@teclib.com>
2018-09-10 17:33:34 -0400

    ci(documentation): add branch and remove unused code
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- ci/scripts/generate_documentation.sh
+++ ci/scripts/generate_documentation.sh
@@ -27,42 +27,17 @@
 #  ------------------------------------------------------------------------------
 #
 
-# # create code coverage report
-# ./gradlew createDebugCoverageReport
-#
-# # move code coverage
-# mv -v app/build/reports/coverage development
-#
-# # move Android test
-# mv -v app/build/reports/androidTests development
-#
-# # rename folders to match respective section on project site
-# mv development/debug development/coverage
-# mv development/androidTests/connected development/test-reports
-#
-# # replace .resources with resource because github doesn't support folders with "_" or "." at the beginning
-# mv development/coverage/.resources development/coverage/resources
-#
-# # find and replace links to the old name of file
-# grep -rl .resources development/coverage/ | xargs sed -i 's|.resources|resources|g'
-#
-# # replace .sessions
-# mv development/coverage/.sessions.html development/coverage/sessions.html
-#
-# # find and replace links to the old name of file
-# grep -rl .sessions.html development/coverage/ | xargs sed -i 's|.sessions.html|sessions.html|g'
-
 # Generate javadoc this folder must be on .gitignore
-javadoc -d ./development/code-documentation -sourcepath ./app/src/main/java -subpackages . -bootclasspath $ANDROID_HOME/platforms/android-28/android.jar
+javadoc -d ./development/code-documentation/"$CIRCLE_BRANCH"/ -sourcepath ./app/src/main/java -subpackages . -bootclasspath $ANDROID_HOME/platforms/android-28/android.jar
 
 # delete the index.html file
-sudo rm ./development/code-documentation/index.html
+sudo rm ./development/code-documentation/"$CIRCLE_BRANCH"/index.html
 
 # rename the overview-summary.html file to index.html
-mv ./development/code-documentation/overview-summary.html ./development/code-documentation/index.html
+mv ./development/code-documentation/"$CIRCLE_BRANCH"/overview-summary.html ./development/code-documentation/"$CIRCLE_BRANCH"/index.html
 
 # find and replace links to the old name of file
-grep -rl overview-summary.html development/code-documentation/ | xargs sed -i 's|overview-summary.html|index.html|g'
+grep -rl overview-summary.html development/code-documentation/"$CIRCLE_BRANCH"/ | xargs sed -i 's|overview-summary.html|index.html|g'
 
 # send development folder to project site with the documentation updated, also removes the folder with old docs
 yarn gh-pages --dist ./development/ --dest ./development/ -m "docs(development): update documentation
