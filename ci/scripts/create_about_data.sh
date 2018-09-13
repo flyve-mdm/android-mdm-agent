a83d59692cee8c9d9918800a4ab02520900df5a8
Rafa Hernandez <rhernandez@teclib.com>
2018-09-13 18:28:01 -0400

    build(about): get version code from manifest
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- ci/scripts/create_about_data.sh
+++ ci/scripts/create_about_data.sh
@@ -31,9 +31,10 @@ export GIT_TAG=$(jq -r ".version" package.json)
 
 # Get version number from package.json
 export GIT_TAG=$(jq -r ".version" package.json)
+export VERSION_CODE=$(grep versionCode app/src/main/AndroidManifest.xml | cut -d\" -f2)
 
 echo "about.version=${GIT_TAG}" > app/src/main/assets/about.properties
-echo "about.build=$CIRCLE_BUILD_NUM" >> app/src/main/assets/about.properties
+echo "about.build=$VERSION_CODE" >> app/src/main/assets/about.properties
 echo "about.date=$(date "+%a %b %d %H:%M:%S %Y")" >> app/src/main/assets/about.properties
 echo "about.commit=${CIRCLE_SHA1:0:7}" >> app/src/main/assets/about.properties
 echo "about.commitFull=$CIRCLE_SHA1" >> app/src/main/assets/about.properties
