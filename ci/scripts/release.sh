4459f6c5adf78b27715d8cba1a2d17747afebc1a
Rafa Hernandez <rhernandez@teclib.com>
2018-09-12 20:12:36 -0400

    ci(release): reorder script
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- ci/scripts/release.sh
+++ ci/scripts/release.sh
@@ -26,8 +26,11 @@
 #  --------------------------------------------------------------------------------
 #
 
+# Get version number from package.json
+export GIT_TAG=$(jq -r ".version" package.json)
+
 # Generate CHANGELOG.md and increment version
-IS_PRERELEASE="$( cut -d '-' -f 2 <<< "$CIRCLE_BRANCH" )";
+IS_PRERELEASE="$( cut -d '-' -f 2 <<< "$GIT_TAG" )";
 
 if [[ $CIRCLE_BRANCH != "$IS_PRERELEASE" ]]; then
 
@@ -57,9 +60,6 @@ git checkout . -f
 
 # Push commits and tags to origin branch
 git push --follow-tags origin $CIRCLE_BRANCH
-
-# Get version number from package.json
-export GIT_TAG=$(jq -r ".version" package.json)
 
 # Create release with conventional-github-releaser
 yarn conventional-github-releaser -p angular -t $GITHUB_TOKEN
