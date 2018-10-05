77abd4014c613ebdc94ca2d6c10a1259b05c50e1
Rafa Hernandez <rhernandez@teclib.com>
2018-10-05 19:56:44 -0400

    feat(policies): add firebase class
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

---
+++ app/src/main/java/org/flyve/mdm/agent/ui/PushPoliciesActivity.java
@@ -0,0 +1,17 @@
+package org.flyve.mdm.agent.firebase;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.flyve.mdm.agent.R;
+
+public class PushPoliciesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_push_policies);
+
+
+    }
+}
