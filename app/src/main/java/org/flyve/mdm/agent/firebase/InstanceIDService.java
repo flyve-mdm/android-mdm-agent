52d760ffd86da1f99abc02a4e8cd68a8773b17e5
Rafa Hernandez <rhernandez@teclib.com>
2018-10-05 19:56:44 -0400

    feat(policies): add firebase class
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

---
+++ app/src/main/java/org/flyve/mdm/agent/firebase/InstanceIDService.java
@@ -0,0 +1,42 @@
+package org.flyve.mdm.agent.firebase;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class InstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "IIDService";
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+    // [END refresh_token]
+
+    /**
+     * Persist token to third-party servers.
+     *
+     * Modify this method to associate the user's FCM InstanceID token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
+     */
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
+    }
+}
