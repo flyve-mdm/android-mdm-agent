40f0c4a7ec81f3da4d4cdb026db8c20514219d99
Rafa Hernandez <rhernandez@teclib.com>
2018-10-05 19:56:44 -0400

    feat(policies): add firebase class
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

---
+++ app/src/main/java/org/flyve/mdm/agent/firebase/MessagingService.java
@@ -0,0 +1,88 @@
+package org.flyve.mdm.agent.firebase;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import org.flyve.mdm.agent.R;
+import org.flyve.mdm.agent.utils.FlyveLog;
+
+public class MessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MessageService";
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // There are two types of messages data messages and notification messages. Data messages are handled
+        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
+        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
+        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
+        // When the user taps on the notification they are returned to the app. Messages containing both notification
+        // and data payloads are treated as notification messages. The Firebase console always sends notification
+        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
+        //
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        sendNotification(remoteMessage.getNotification().getBody());
+
+    }
+
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     */
+
+    private void sendNotification(String message) {
+
+        FlyveLog.d("Notification: " + message);
+        Intent resultIntent = new Intent(this, org.flyve.mdm.agent.firebase.PushPoliciesActivity.class);
+        resultIntent.putExtra("payload", message);
+        resultIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent piResult = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_notification_white)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setContentTitle(getResources().getString(R.string.app_name))
+                .setContentText(message)
+                .setSound(defaultSoundUri)
+                .setAutoCancel(true)
+                .setContentIntent(piResult);
+
+        if (Build.VERSION.SDK_INT < 16) {
+            builder.setContentText(message);
+        } else {
+            builder.setStyle(new NotificationCompat.BigTextStyle().bigText(message));
+        }
+
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(121, builder.build());
+    }
+}
