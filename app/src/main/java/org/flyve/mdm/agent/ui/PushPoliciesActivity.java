9257d1aeedf8bc4c09a2eafef160abce72f98d62
Rafa Hernandez <rhernandez@teclib.com>
2018-10-08 17:46:45 -0400

    feat(policies): add screen for notification
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

--- app/src/main/java/org/flyve/mdm/agent/ui/PushPoliciesActivity.java
+++ app/src/main/java/org/flyve/mdm/agent/ui/PushPoliciesActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import org.flyve.mdm.agent.R;
+import org.flyve.mdm.agent.policies.PasswordEnablePolicy;
+import org.flyve.mdm.agent.utils.FlyveLog;
 
 public class PushPoliciesActivity extends AppCompatActivity {
 
@@ -12,6 +14,18 @@ public class PushPoliciesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_push_policies);
 
+        String payload = getIntent().getStringExtra("payload");
 
+        PasswordEnablePolicy airplaneModePolicy = new PasswordEnablePolicy(getApplicationContext());
+        airplaneModePolicy.setMqttEnable(false);
+        airplaneModePolicy.setValue(true);
+        airplaneModePolicy.setPriority(1);
+        FlyveLog.d("Password policy: " + payload);
+
+        try {
+            airplaneModePolicy.execute();
+        } catch (Exception ex) {
+            FlyveLog.d(ex.getMessage());
+        }
     }
 }
