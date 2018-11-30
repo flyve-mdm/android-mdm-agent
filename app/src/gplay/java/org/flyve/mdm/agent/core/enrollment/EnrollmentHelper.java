ab1dca79d1617d0f59764cc8b9b12ecaac151b7b
Rafa Hernandez <rhernandez@teclib.com>
2018-11-30 15:32:30 -0400

    feat(flavor): implement gplay flavor
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

---
+++ app/src/gplay/java/org/flyve/mdm/agent/core/enrollment/EnrollmentHelper.java
@@ -0,0 +1,351 @@
+/*
+ * Copyright Teclib. All rights reserved.
+ *
+ * Flyve MDM is a mobile device management software.
+ *
+ * Flyve MDM is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Flyve MDM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Rafael Hernandez
+ * @copyright Copyright Teclib. All rights reserved.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/android-mdm-agent
+ * @link      https://flyve-mdm.com
+ * ------------------------------------------------------------------------------
+ */
+
+package org.flyve.mdm.agent.core.enrollment;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+
+import org.flyve.mdm.agent.R;
+import org.flyve.mdm.agent.core.CommonErrorType;
+import org.flyve.mdm.agent.core.Routes;
+import org.flyve.mdm.agent.data.database.MqttData;
+import org.flyve.mdm.agent.utils.ConnectionHTTP;
+import org.flyve.mdm.agent.utils.FlyveLog;
+import org.flyve.mdm.agent.utils.Helpers;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+import static org.flyve.mdm.agent.utils.ConnectionHTTP.getSyncWebData;
+
+public class EnrollmentHelper {
+
+    private static Handler uiHandler;
+    
+    private static final String SESSION_TOKEN = "Session-Token";
+    private static final String ACCEPT = "Accept";
+    private static final String APPLICATION_JSON = "application/json";
+    private static final String CONTENT_TYPE = "Content-Type";
+    private static final String CHARSET = "charset=UTF-8";
+    private static final String USER_AGENT = "User-Agent";
+    private static final String FLYVE_MDM = "Flyve MDM";
+    private static final String REFERER = "Referer";
+    private String sessionToken = "";
+    private String data = "";
+
+    static {
+        uiHandler = new Handler(Looper.getMainLooper());
+    }
+
+    private static void runOnUI(Runnable runnable) {
+        uiHandler.post(runnable);
+    }
+
+    private Context context;
+    private MqttData cache;
+    private Routes routes;
+
+    /**
+     * This constructor loads the context of the current class
+     * @param context of the class
+     */
+    public EnrollmentHelper(Context context) {
+        this.context = context;
+        cache = new MqttData(context);
+        routes = new Routes(context);
+    }
+
+    /**
+     * Manage the errors 
+     * @param error
+     * @return string the message of the error
+     */
+    private String manageError(String error) {
+        String errorMessage = "";
+
+        if(error.contains("EXCEPTION_HTTP") || error.contains("ERROR")) {
+            FlyveLog.e(this.getClass().getName() + ", manageError", error);
+
+            errorMessage = error;
+
+            if(error.contains("EXCEPTION_HTTP")) {
+                return errorMessage;
+            }
+
+            // Manage error from backend
+            // Example: ["ERROR_SESSION_TOKEN_MISSING","parameter session_token is missing or empty; View documentation in your browser at https://dev.flyve.org/glpi/apirest.php/#ERROR_SESSION_TOKEN_MISSING"]
+            try {
+                JSONArray jError = new JSONArray(error);
+                errorMessage = jError.getString(1);
+
+                String[] errorArray = errorMessage.split(";");
+                if(errorArray.length > 0) {
+                    errorMessage = errorArray[0];
+                }
+            } catch (Exception ex) {
+                FlyveLog.e(this.getClass().getName() + ", manageError", ex.getMessage());
+            }
+
+            return errorMessage;
+        }
+
+        return errorMessage;
+    }
+
+    /**
+     * Get session token
+     */
+    public void getActiveSessionToken(final EnrollCallBack callback) {
+
+        Thread t = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                String profileId = "";
+                JSONObject jsonSession;
+                HashMap<String, String> header = new HashMap();
+                header.put("user_token", cache.getUserToken());
+
+                try {
+                    // STEP 1 get session token
+                    data = getSyncWebData(routes.initSession(cache.getUserToken()), "GET", header);
+
+                    final String errorMessage = manageError(data);
+                    if(!errorMessage.equals("")) {
+                        EnrollmentHelper.runOnUI(new Runnable() {
+                            public void run() {
+                                callback.onError(CommonErrorType.ENROLLMENT_HELPER_INITSESSION, errorMessage);
+                            }
+                        });
+                        return;
+                    }
+
+                    jsonSession = new JSONObject(data);
+                    sessionToken = jsonSession.getString("session_token");
+                    cache.setSessionToken(sessionToken);
+
+                } catch (final Exception ex) {
+                    FlyveLog.e(this.getClass().getName() + ", getActiveSessionToken", ex.getMessage());
+                    EnrollmentHelper.runOnUI(new Runnable() {
+                        public void run() {
+                            callback.onError(CommonErrorType.ENROLLMENT_HELPER_INITSESSION, context.getString(R.string.wrong_json_format, data, ex.getMessage()));
+                        }
+                    });
+                }
+
+                try {
+                    // STEP 2 get full session information
+                    header.put(SESSION_TOKEN, sessionToken);
+                    header.put(ACCEPT,APPLICATION_JSON);
+                    header.put(CONTENT_TYPE,APPLICATION_JSON + ";" + CHARSET);
+                    header.put(USER_AGENT,FLYVE_MDM);
+                    header.put(REFERER,routes.getFullSession());
+
+                    data = getSyncWebData(routes.getFullSession(), "GET", header);
+                    final String errorMessageFullSession = manageError(data);
+                    if(!errorMessageFullSession.equals("")) {
+                        EnrollmentHelper.runOnUI(new Runnable() {
+                            public void run() {
+                                callback.onError(CommonErrorType.ENROLLMENT_HELPER_FULLSESSION, errorMessageFullSession);
+                            }
+                        });
+                        return;
+                    }
+
+                    JSONObject jsonFullSession = new JSONObject(data);
+                    jsonSession = jsonFullSession.getJSONObject("session");
+                    profileId = jsonSession.getString("plugin_flyvemdm_guest_profiles_id");
+
+                    cache.setProfileId(profileId);
+
+                } catch (final Exception ex) {
+                    FlyveLog.e(this.getClass().getName() + ", getActiveSessionToken", ex.getMessage());
+                    EnrollmentHelper.runOnUI(new Runnable() {
+                        public void run() {
+                            callback.onError(CommonErrorType.ENROLLMENT_HELPER_FULLSESSION, context.getString(R.string.wrong_json_format, data, ex.getMessage()));
+                        }
+                    });
+                }
+
+                try {
+                    // STEP 3 Activated the profile
+                    data = getSyncWebData(routes.changeActiveProfile(profileId), "POST", header);
+                    final String errorActiveProfile = manageError(data);
+                    if(!errorActiveProfile.equals("")) {
+                        EnrollmentHelper.runOnUI(new Runnable() {
+                            public void run() {
+                                callback.onError(CommonErrorType.ENROLLMENT_HELPER_CHANGEACTIVEPROFILE, errorActiveProfile);
+                            }
+                        });
+                    } else {
+                        // Success
+                        EnrollmentHelper.runOnUI(new Runnable() {
+                            public void run() {
+                                callback.onSuccess(sessionToken);
+                            }
+                        });
+                    }
+                } catch (final Exception ex) {
+                    FlyveLog.e(this.getClass().getName() + ", getActiveSessionToken", ex.getMessage());
+                    EnrollmentHelper.runOnUI(new Runnable() {
+                        public void run() {
+                            callback.onError(CommonErrorType.ENROLLMENT_HELPER_CHANGEACTIVEPROFILE, context.getString(R.string.wrong_json_format, data, ex.getMessage()));
+                        }
+                    });
+                }
+            }
+        });
+        t.start();
+    }
+
+    /**
+     * Get the information to enroll
+     * @param payload the information of the phone
+     * @param callback
+     */
+    public void enrollment(final JSONObject payload, final EnrollCallBack callback) {
+        Thread t = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                HashMap<String, String> header = new HashMap();
+                header.put(SESSION_TOKEN, cache.getSessionToken());
+
+                header.put(ACCEPT,APPLICATION_JSON);
+                header.put(CONTENT_TYPE,APPLICATION_JSON + ";" + CHARSET);
+
+                JSONObject input;
+                try {
+                    input = new JSONObject();
+                    input.put("input", payload);
+                    FlyveLog.d("Flyve MDM Payload: " + input);
+                } catch (final Exception ex) {
+                    EnrollmentHelper.runOnUI(new Runnable() {
+                        public void run() {
+                            callback.onError(CommonErrorType.ENROLLMENT_HELPER_INPUT_PAYLOAD, ex.getMessage());
+                        }
+                    });
+                    return;
+                }
+
+                // Sending the payload to the backend
+                String data = getSyncWebData(routes.pluginFlyvemdmAgent(), input, header);
+                FlyveLog.d("Payload return: " + data);
+
+                if(data.contains("ERROR")){
+                    final String msgError = manageError(data);
+                    FlyveLog.e(this.getClass().getName() + ", enrollment", msgError + " - Device serial: " + Helpers.getDeviceSerial());
+
+                    EnrollmentHelper.runOnUI(new Runnable() {
+                        public void run() {
+                            callback.onError(CommonErrorType.ENROLLMENT_HELPER_REQUEST_PAYLOAD, msgError);
+                        }
+                    });
+                } else {
+                    String agentId;
+                    try {
+                        JSONObject jsonAgent = new JSONObject(data);
+                        agentId = jsonAgent.getString("id");
+                    } catch (final Exception ex) {
+                        EnrollmentHelper.runOnUI(new Runnable() {
+                            public void run() {
+                                callback.onError(CommonErrorType.ENROLLMENT_HELPER_AGENT_ID, ex.getMessage());
+                            }
+                        });
+                        return;
+                    }
+
+                    header = new HashMap();
+                    header.put(SESSION_TOKEN, cache.getSessionToken());
+                    header.put(ACCEPT,APPLICATION_JSON);
+                    header.put(CONTENT_TYPE,APPLICATION_JSON + ";" + CHARSET);
+                    header.put(USER_AGENT,FLYVE_MDM);
+                    header.put(REFERER,routes.pluginFlyvemdmAgent());
+
+                    String dataAgent = ConnectionHTTP.getSyncWebData(routes.pluginFlyvemdmAgent(agentId), "GET", header);
+
+                    try {
+                        JSONObject jsonObject = new JSONObject(dataAgent);
+
+                        String mbroker = jsonObject.getString("broker");
+                        String mport = jsonObject.getString("port");
+                        String mssl = jsonObject.getString("tls");
+                        String mtopic = jsonObject.getString("topic");
+                        String mpassword = jsonObject.getString("mqttpasswd");
+                        String mcert = jsonObject.getString("certificate");
+                        String mNameEmail = jsonObject.getString("name");
+                        int mComputersId = jsonObject.getInt("computers_id");
+                        int mId = jsonObject.getInt("id");
+                        FlyveLog.d("Id: " + mId);
+                        int mEntitiesId = jsonObject.getInt("entities_id");
+                        int mFleetId = jsonObject.getInt("plugin_flyvemdm_fleets_id");
+
+                        cache.setAgentId(agentId);
+                        cache.setBroker(mbroker);
+                        cache.setPort(mport);
+                        cache.setTls(mssl);
+                        cache.setTopic(mtopic);
+                        cache.setMqttUser(Helpers.getDeviceSerial());
+                        cache.setMqttPasswd(mpassword);
+                        cache.setCertificate(mcert);
+                        cache.setName(mNameEmail);
+                        cache.setComputersId(String.valueOf(mComputersId));
+                        cache.setEntitiesId(String.valueOf(mEntitiesId));
+                        cache.setPluginFlyvemdmFleetsId(String.valueOf(mFleetId));
+
+                    } catch (final Exception ex) {
+                        EnrollmentHelper.runOnUI(new Runnable() {
+                            public void run() {
+                                callback.onError(CommonErrorType.ENROLLMENT_HELPER_DATA_AGENT, ex.getMessage());
+                            }
+                        });
+                        return;
+                    }
+
+                    EnrollmentHelper.runOnUI(new Runnable() {
+                        public void run() {
+                            callback.onSuccess("success");
+                        }
+                    });
+                }
+            }
+        });
+        t.start();
+    }
+
+    /**
+     * Create X509 certificate
+     */
+    public void createX509cert(final EnrollCallBack callback) {
+        // is not require on gplay always success
+        callback.onSuccess("true");
+    }
+
+    public interface EnrollCallBack {
+        void onSuccess(String data);
+        void onError(int type, String error);
+    }
+}
---
+++ app/src/gplay/java/org/flyve/mdm/agent/policies/BasePolicies.java
@@ -0,0 +1,168 @@
+package org.flyve.mdm.agent.policies;
+
+/*
+ *   Copyright  2018 Teclib. All rights reserved.
+ *
+ *   This file is part of flyve-mdm-android
+ *
+ * flyve-mdm-android is a subproject of Flyve MDM. Flyve MDM is a mobile
+ * device management software.
+ *
+ * Flyve MDM is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Flyve MDM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    rafael hernandez
+ * @date      15/5/18
+ * @copyright Copyright  2018 Teclib. All rights reserved.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/flyve-mdm-android
+ * @link      https://flyve-mdm.com
+ * ------------------------------------------------------------------------------
+ */
+
+import android.content.Context;
+
+import org.flyve.mdm.agent.data.database.PoliciesData;
+import org.flyve.mdm.agent.data.database.entity.Policies;
+import org.flyve.mdm.agent.utils.FlyveLog;
+
+public abstract class BasePolicies {
+
+    private static final String ERROR = "ERROR";
+    private static final String MQTT_SEND = "MQTT Send";
+
+    private static final String MQTT_FEEDBACK_PENDING = "pending";
+    private static final String MQTT_FEEDBACK_RECEIVED = "received";
+    private static final String MQTT_FEEDBACK_DONE = "done";
+    private static final String MQTT_FEEDBACK_FAILED = "failed";
+    private static final String MQTT_FEEDBACK_CANCELED = "canceled";
+    private static final String MQTT_FEEDBACK_WAITING = "waiting";
+
+    private boolean enableLog;
+    protected Context context;
+    protected String policyName;
+    protected PoliciesData data;
+    protected Object policyValue;
+    protected int policyPriority;
+
+    private boolean mqttEnable;
+    private String mqttTopic;
+    private String mqttTaskId;
+
+    public BasePolicies(Context context, String name) {
+        this.context = context;
+        this.policyName = name;
+        this.data = new PoliciesData(context);
+        this.mqttEnable = true;
+
+        Policies policies = data.getValue(this.policyName);
+        if(policies!=null) {
+            this.policyValue = policies.value;
+            this.policyPriority = policies.priority;
+        }
+    }
+
+    public void setMqttEnable(boolean enable) {
+        this.mqttEnable = enable;
+    }
+
+    public void setValue(Object value) {
+        this.policyValue = value;
+        storage();
+    }
+
+    public void setPriority(int priority) {
+        this.policyPriority = priority;
+        storage();
+    }
+
+    public void setLog(Boolean enable) {
+        this.enableLog = enable;
+    }
+
+    private void storage()  {
+        if(!policyName.isEmpty() && !this.policyValue.toString().isEmpty()) {
+            data.setValue(this.policyName, String.valueOf(this.policyValue), this.policyPriority);
+        }
+    }
+
+    public void remove() {
+        if(!policyName.isEmpty()) {
+            data.removeValue(policyName, policyPriority);
+        }
+    }
+
+    private void mqttSendTaskStatus(String mqttTopic, String taskId, String status) {
+        String topic = mqttTopic + "/Status/Task/" + taskId;
+        byte[] encodedPayload;
+        try {
+            String payload = "{ \"status\": \"" + status + "\" }";
+            encodedPayload = payload.getBytes("UTF-8");
+
+            //TODO send the task status by http
+
+            Log(MQTT_SEND, "Policy Status", "TaskID: " + taskId + " Status: " + status);
+        } catch (Exception ex) {
+            FlyveLog.e(this.getClass().getName() + ", mqttSendTaskStatus", ex.getMessage());
+
+            // send broadcast
+            Log(ERROR, "Error sending status", ex.getMessage());
+        }
+    }
+
+    private void mqttResponse(String status) {
+        if(mqttEnable) {
+            this.mqttSendTaskStatus(this.mqttTopic, this.mqttTaskId, status);
+        }
+    }
+
+    protected void Log(String type, String title, String message){
+        // write log file
+        if(enableLog) {
+            FlyveLog.f(type, title, message);
+        }
+    }
+
+    private void validate() throws PoliciesException {
+        if(this.policyName.isEmpty()){
+            throw new PoliciesException("provide a name for the policy");
+        }
+
+        if(this.policyValue.toString().isEmpty()){
+            throw new PoliciesException("provide a value for the policy");
+        }
+    }
+
+    public void execute() throws PoliciesException {
+        validate();
+        Log("Execute Policy", "Policy " + this.policyName, "Start the policy: " + this.policyName + "\nvalue: " + this.policyValue + "\npriority:" + this.policyPriority);
+        boolean status = process();
+        if(status) {
+            policyDone();
+        } else {
+            policyFail();
+        }
+    }
+
+    protected void policyDone() {
+        if(mqttEnable) {
+            mqttResponse(MQTT_FEEDBACK_DONE);
+        }
+    }
+
+    protected void policyFail() {
+        Log("Policy ERROR", "Policy " + this.policyName,"Policy Fail: " + this.policyName + "\nvalue: " + this.policyValue + "\npriority: " + this.policyPriority);
+        if(mqttEnable) {
+            mqttResponse(MQTT_FEEDBACK_FAILED);
+        }
+    }
+
+    protected abstract boolean process();
+}
