d458926a74895e7f89cbc5ecbea067173617a742
Rafa Hernandez <rhernandez@teclib.com>
2018-12-14 11:28:03 -0400

    refactor(mqtt): add topics table
    Signed-off-by: Rafa Hernandez <rhernandez@teclib.com>

---
+++ app/src/main/java/org/flyve/mdm/agent/data/database/TopicsData.java
@@ -0,0 +1,51 @@
+/*
+ *   Copyright  Teclib. All rights reserved.
+ *
+ *   This file is part of flyve-mdm-android
+ *
+ * flyve-mdm-android is a subproject of Flyve MDM. Flyve MDM is a mobile
+ * device management software.
+ *
+ * Flyve MDM is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Flyve MDM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    @rafaelje
+ * @copyright Copyright  Teclib. All rights reserved.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/flyve-mdm-android
+ * @link      https://flyve-mdm.com
+ * ------------------------------------------------------------------------------
+ */
+
+package org.flyve.mdm.agent.data.database;
+
+import android.content.Context;
+
+import org.flyve.mdm.agent.data.database.entity.Topics;
+import org.flyve.mdm.agent.data.database.setup.AppDataBase;
+
+import java.util.List;
+
+public class TopicsData {
+
+    private AppDataBase dataBase;
+
+    public TopicsData(Context context) {
+        dataBase = AppDataBase.getAppDatabase(context);
+    }
+
+    public List<Topics> getAllTopics() {
+        return dataBase.TopicsDao().loadAll();
+    }
+
+    public void deleteAll() {
+        dataBase.TopicsDao().deleteAll();
+    }
+}
---
+++ app/src/main/java/org/flyve/mdm/agent/data/database/entity/Topics.java
@@ -0,0 +1,43 @@
+package org.flyve.mdm.agent.data.database.entity;
+
+/*
+ *   Copyright  2018 Teclib. All rights reserved.
+ *
+ *   This file is part of flyve-mdm-android
+ *
+ * flyve-mdm-android is a subproject of Flyve MDM. Flyve MDM is a mobile
+ * device management software.
+ *
+ * Flyve MDM is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Flyve MDM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Rafael Hernandez
+ * @date      7/4/18
+ * @copyright Copyright  2018 Teclib. All rights reserved.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/flyve-mdm-android
+ * @link      https://flyve-mdm.com
+ * ------------------------------------------------------------------------------
+ */
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "topics")
+public class Topics {
+
+    @PrimaryKey (autoGenerate = true)
+    public int id;
+
+    @ColumnInfo (name = "topic")
+    public String topic;
+
+}
