1e51e1b9050b029b6410f46f90509389444f6342
Alexander Salas Bastidas <asalas@teclib.com>
2019-07-22 17:33:16 +0200

    ci(circleci): setup ruby gems shell script

---
+++ ci/scripts/setup_ruby_gems.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#
+#  LICENSE
+#
+#  This file is part of Flyve MDM Agent for Android.
+#
+#  Flyve MDM Agent for Android is a subproject of Flyve MDM. Flyve MDM is a mobile
+#  device management software.
+#
+#  Flyve MDM is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 3
+#  of the License, or (at your option) any later version.
+#
+#  Flyve MDM Agent for Android is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  --------------------------------------------------------------------------------
+#  @author    Alexander Salas Bastidas - <asalas@teclib.com>
+#  @copyright Copyright (c) Teclib' 2019
+#  @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+#  @link      https://github.com/flyve-mdm/android-mdm-agent/
+#  @link      http://flyve.org/android-mdm-agent/
+#  @link      https://flyve-mdm.com/
+#  --------------------------------------------------------------------------------
+#
+
+# Maintenance commands
+sudo apt-get update
+
+# install gems
+sudo apt-get install ruby-full build-essential
+
+# update Rubygems
+sudo gem update --system
+
+# install fastlane
+sudo gem install fastlane
+
+# update bundler
+sudo gem install bundler
+
+# update Gemfile.lock
+sudo bundler update --bundler
+
+# check bundle
+bundle check || bundle install --path vendor/bundle
